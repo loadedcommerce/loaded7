f2e236d5b4b12ae196c6ed7490daaa0975a31b4f
datazen <scott.logsdon@loadedcommerce.com>
2013-01-06 10:52:29 -0500

    [IC] removed whitespace

--- catalog/includes/modules/order_total/sub_total.php
+++ catalog/includes/modules/order_total/sub_total.php
@@ -1,42 +1,42 @@
-<?php
-/*
-  $Id$
-
-  LoadedCommerce, Open Source E-Commerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2006 LoadedCommerce
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License v2 (1991)
-  as published by the Free Software Foundation.
-*/
-
-  class lC_OrderTotal_sub_total extends lC_OrderTotal {
-    var $output;
-
-    var $_title,
-        $_code = 'sub_total',
-        $_status = false,
-        $_sort_order;
-
-    function lC_OrderTotal_sub_total() {
-      global $lC_Language;
-
-      $this->output = array();
-
-      $this->_title = $lC_Language->get('order_total_subtotal_title');
-      $this->_description = $lC_Language->get('order_total_subtotal_description');
-      $this->_status = (defined('MODULE_ORDER_TOTAL_SUBTOTAL_STATUS') && (MODULE_ORDER_TOTAL_SUBTOTAL_STATUS == 'true') ? true : false);
-      $this->_sort_order = (defined('MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER') ? MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER : null);
-    }
-
-    function process() {
-      global $lC_ShoppingCart, $lC_Currencies;
-
-      $this->output[] = array('title' => $this->_title . ':',
-                              'text' => $lC_Currencies->format($lC_ShoppingCart->getSubTotal()),
-                              'value' => $lC_ShoppingCart->getSubTotal());
-    }
-  }
-?>
+<?php
+/*
+  $Id$
+
+  LoadedCommerce, Open Source E-Commerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2006 LoadedCommerce
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License v2 (1991)
+  as published by the Free Software Foundation.
+*/
+
+  class lC_OrderTotal_sub_total extends lC_OrderTotal {
+    var $output;
+
+    var $_title,
+        $_code = 'sub_total',
+        $_status = false,
+        $_sort_order;
+
+    function lC_OrderTotal_sub_total() {
+      global $lC_Language;
+
+      $this->output = array();
+
+      $this->_title = $lC_Language->get('order_total_subtotal_title');
+      $this->_description = $lC_Language->get('order_total_subtotal_description');
+      $this->_status = (defined('MODULE_ORDER_TOTAL_SUBTOTAL_STATUS') && (MODULE_ORDER_TOTAL_SUBTOTAL_STATUS == 'true') ? true : false);
+      $this->_sort_order = (defined('MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER') ? MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER : null);
+    }
+
+    function process() {
+      global $lC_ShoppingCart, $lC_Currencies;
+
+      $this->output[] = array('title' => $this->_title . ':',
+                              'text' => $lC_Currencies->format($lC_ShoppingCart->getSubTotal()),
+                              'value' => $lC_ShoppingCart->getSubTotal());
+    }
+  }
+?>
--- catalog/includes/modules/payment/cod.php
+++ catalog/includes/modules/payment/cod.php
@@ -1,71 +1,71 @@
-<?php
-/*
-  $Id$
-
-  LoadedCommerce, Open Source E-Commerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2006 LoadedCommerce
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License v2 (1991)
-  as published by the Free Software Foundation.
-*/
-
-  class lC_Payment_cod extends lC_Payment {
-    var $_title,
-        $_code = 'cod',
-        $_status = false,
-        $_sort_order,
-        $_order_id;
-
-    function lC_Payment_cod() {
-      global $lC_Database, $lC_Language, $lC_ShoppingCart;
-
-      $this->_title = $lC_Language->get('payment_cod_title');
-      $this->_method_title = $lC_Language->get('payment_cod_method_title');
-      $this->_status = (MODULE_PAYMENT_COD_STATUS == '1') ? true : false;
-      $this->_sort_order = MODULE_PAYMENT_COD_SORT_ORDER;
-
-      if ($this->_status === true) {
-        if ((int)MODULE_PAYMENT_COD_ORDER_STATUS_ID > 0) {
-          $this->order_status = MODULE_PAYMENT_COD_ORDER_STATUS_ID;
-        }
-
-        if ((int)MODULE_PAYMENT_COD_ZONE > 0) {
-          $check_flag = false;
-
-          $Qcheck = $lC_Database->query('select zone_id from :table_zones_to_geo_zones where geo_zone_id = :geo_zone_id and zone_country_id = :zone_country_id order by zone_id');
-          $Qcheck->bindTable(':table_zones_to_geo_zones', TABLE_ZONES_TO_GEO_ZONES);
-          $Qcheck->bindInt(':geo_zone_id', MODULE_PAYMENT_COD_ZONE);
-          $Qcheck->bindInt(':zone_country_id', $lC_ShoppingCart->getBillingAddress('country_id'));
-          $Qcheck->execute();
-
-          while ($Qcheck->next()) {
-            if ($Qcheck->valueInt('zone_id') < 1) {
-              $check_flag = true;
-              break;
-            } elseif ($Qcheck->valueInt('zone_id') == $lC_ShoppingCart->getBillingAddress('zone_id')) {
-              $check_flag = true;
-              break;
-            }
-          }
-
-          if ($check_flag == false) {
-            $this->_status = false;
-          }
-        }
-      }
-    }
-
-    function selection() {
-      return array('id' => $this->_code,
-                   'module' => $this->_method_title);
-    }
-
-    function process() {
-      $this->_order_id = lC_Order::insert();
-      lC_Order::process($this->_order_id, $this->order_status);
-    }
-  }
-?>
+<?php
+/*
+  $Id$
+
+  LoadedCommerce, Open Source E-Commerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2006 LoadedCommerce
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License v2 (1991)
+  as published by the Free Software Foundation.
+*/
+
+  class lC_Payment_cod extends lC_Payment {
+    var $_title,
+        $_code = 'cod',
+        $_status = false,
+        $_sort_order,
+        $_order_id;
+
+    function lC_Payment_cod() {
+      global $lC_Database, $lC_Language, $lC_ShoppingCart;
+
+      $this->_title = $lC_Language->get('payment_cod_title');
+      $this->_method_title = $lC_Language->get('payment_cod_method_title');
+      $this->_status = (MODULE_PAYMENT_COD_STATUS == '1') ? true : false;
+      $this->_sort_order = MODULE_PAYMENT_COD_SORT_ORDER;
+
+      if ($this->_status === true) {
+        if ((int)MODULE_PAYMENT_COD_ORDER_STATUS_ID > 0) {
+          $this->order_status = MODULE_PAYMENT_COD_ORDER_STATUS_ID;
+        }
+
+        if ((int)MODULE_PAYMENT_COD_ZONE > 0) {
+          $check_flag = false;
+
+          $Qcheck = $lC_Database->query('select zone_id from :table_zones_to_geo_zones where geo_zone_id = :geo_zone_id and zone_country_id = :zone_country_id order by zone_id');
+          $Qcheck->bindTable(':table_zones_to_geo_zones', TABLE_ZONES_TO_GEO_ZONES);
+          $Qcheck->bindInt(':geo_zone_id', MODULE_PAYMENT_COD_ZONE);
+          $Qcheck->bindInt(':zone_country_id', $lC_ShoppingCart->getBillingAddress('country_id'));
+          $Qcheck->execute();
+
+          while ($Qcheck->next()) {
+            if ($Qcheck->valueInt('zone_id') < 1) {
+              $check_flag = true;
+              break;
+            } elseif ($Qcheck->valueInt('zone_id') == $lC_ShoppingCart->getBillingAddress('zone_id')) {
+              $check_flag = true;
+              break;
+            }
+          }
+
+          if ($check_flag == false) {
+            $this->_status = false;
+          }
+        }
+      }
+    }
+
+    function selection() {
+      return array('id' => $this->_code,
+                   'module' => $this->_method_title);
+    }
+
+    function process() {
+      $this->_order_id = lC_Order::insert();
+      lC_Order::process($this->_order_id, $this->order_status);
+    }
+  }
+?>
