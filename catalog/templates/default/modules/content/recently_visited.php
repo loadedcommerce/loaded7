23229be05e9fef9d2fa4df9f286288355e77fb23
datazen <scott.logsdon@loadedcommerce.com>
2013-01-06 10:05:06 -0500

    [IC] removed whitespace

--- catalog/templates/default/modules/content/recently_visited.php
+++ catalog/templates/default/modules/content/recently_visited.php
@@ -1,20 +1,20 @@
-<?php
-/*
-  $Id: recently_visited.php v1.0 2013-01-01 datazen $
-
-  LoadedCommerce, Innovative eCommerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2013 Loaded Commerce, LLC
-
-  @author     LoadedCommerce Team
-  @copyright  (c) 2013 LoadedCommerce Team
-  @license    http://loadedcommerce.com/license.html
-*/
-?>
-<!--recently_visited start-->
-<div class="products_list products_slider">
-  <h2 class="sub_title"><?php echo $lC_Box->getTitle(); ?></h2>
-  <?php echo $lC_Box->getContent(); ?>
-</div>
+<?php
+/*
+  $Id: recently_visited.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+?>
+<!--recently_visited start-->
+<div class="products_list products_slider">
+  <h2 class="sub_title"><?php echo $lC_Box->getTitle(); ?></h2>
+  <?php echo $lC_Box->getContent(); ?>
+</div>
 <!--recently_visited end-->
