349d2ecca3294e524c1d97529f316948c638c742
datazen <scott.logsdon@loadedcommerce.com>
2013-01-06 10:19:29 -0500

    [IC] removed whitespace

--- catalog/templates/default/content/index/index.php
+++ catalog/templates/default/content/index/index.php
@@ -1,19 +1,19 @@
-<?php
-/*
-  $Id: index.php v1.0 2013-01-01 datazen $
-
-  LoadedCommerce, Innovative eCommerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2013 Loaded Commerce, LLC
-
-  @author     LoadedCommerce Team
-  @copyright  (c) 2013 LoadedCommerce Team
-  @license    http://loadedcommerce.com/license.html
-*/ 
-?>
-<!--Mainpage Content Starts-->
-
-<!-- Mainpage content goes here -->
-
+<?php
+/*
+  $Id: index.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/ 
+?>
+<!--Mainpage Content Starts-->
+
+<!-- Mainpage content goes here -->
+
 <!--Mainpage Content Ends-->
