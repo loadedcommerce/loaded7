8c989fcb9cf04813cc0b31281bd334896c226c89
datazen <scott.logsdon@loadedcommerce.com>
2013-01-06 10:31:53 -0500

    [IC] removed whitespace

--- catalog/includes/filenames.php
+++ catalog/includes/filenames.php
@@ -1,23 +1,23 @@
-<?php
-/*
-  $Id: filenames.php v1.0 2013-01-01 datazen $
-
-  LoadedCommerce, Innovative eCommerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2013 Loaded Commerce, LLC
-
-  @author     LoadedCommerce Team
-  @copyright  (c) 2013 LoadedCommerce Team
-  @license    http://loadedcommerce.com/license.html
-*/
-// define the filenames used in the project
-  define('FILENAME_ACCOUNT', 'account.php');
-  define('FILENAME_CHECKOUT', 'checkout.php');
-  define('FILENAME_DEFAULT', 'index.php');
-  define('FILENAME_DOWNLOAD', 'download.php');
-  define('FILENAME_INFO', 'info.php');
-  define('FILENAME_PRODUCTS', 'products.php');
-  define('FILENAME_REDIRECT', 'redirect.php');
-  define('FILENAME_SEARCH', 'search.php');
+<?php
+/*
+  $Id: filenames.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+// define the filenames used in the project
+  define('FILENAME_ACCOUNT', 'account.php');
+  define('FILENAME_CHECKOUT', 'checkout.php');
+  define('FILENAME_DEFAULT', 'index.php');
+  define('FILENAME_DOWNLOAD', 'download.php');
+  define('FILENAME_INFO', 'info.php');
+  define('FILENAME_PRODUCTS', 'products.php');
+  define('FILENAME_REDIRECT', 'redirect.php');
+  define('FILENAME_SEARCH', 'search.php');
 ?>
