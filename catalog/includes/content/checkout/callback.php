327a79b2d04cc3ae9d5abaa62254f653f2dafbc2
datazen <scott.logsdon@loadedcommerce.com>
2013-01-06 11:11:15 -0500

    [IC] removed whitespace

--- catalog/includes/content/checkout/callback.php
+++ catalog/includes/content/checkout/callback.php
@@ -1,35 +1,35 @@
-<?php
-/*
-  $Id: callback.php v1.0 2013-01-01 datazen $
-
-  LoadedCommerce, Innovative eCommerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2013 Loaded Commerce, LLC
-
-  @author     LoadedCommerce Team
-  @copyright  (c) 2013 LoadedCommerce Team
-  @license    http://loadedcommerce.com/license.html
-*/
-
-  class lC_Checkout_Callback extends lC_Template {
-
-    /* Private variables */
-    var $_module = 'callback';
-
-    /* Class constructor */
-    function lC_Checkout_Callback() {
-      if (isset($_GET['module']) && (empty($_GET['module']) === false)) {
-        if (file_exists('includes/modules/payment/' . $_GET['module'] . '.php')) {
-          include('includes/classes/order.php');
-          include('includes/classes/payment.php');
-          include('includes/modules/payment/' . $_GET['module'] . '.php');
-          $module = 'lC_Payment_' . $_GET['module'];
-          $module = new $module();
-          $module->callback();
-        }
-      }
-      exit;
-    }
-  }
+<?php
+/*
+  $Id: callback.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+
+  class lC_Checkout_Callback extends lC_Template {
+
+    /* Private variables */
+    var $_module = 'callback';
+
+    /* Class constructor */
+    function lC_Checkout_Callback() {
+      if (isset($_GET['module']) && (empty($_GET['module']) === false)) {
+        if (file_exists('includes/modules/payment/' . $_GET['module'] . '.php')) {
+          include('includes/classes/order.php');
+          include('includes/classes/payment.php');
+          include('includes/modules/payment/' . $_GET['module'] . '.php');
+          $module = 'lC_Payment_' . $_GET['module'];
+          $module = new $module();
+          $module->callback();
+        }
+      }
+      exit;
+    }
+  }
 ?>
--- catalog/includes/content/checkout/payment.php
+++ catalog/includes/content/checkout/payment.php
@@ -1,100 +1,100 @@
-<?php
-/*
-  $Id: payment.php v1.0 2013-01-01 datazen $
-
-  LoadedCommerce, Innovative eCommerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2013 Loaded Commerce, LLC
-
-  @author     LoadedCommerce Team
-  @copyright  (c) 2013 LoadedCommerce Team
-  @license    http://loadedcommerce.com/license.html
-*/
-
-  require('includes/classes/address_book.php');
-
-  class lC_Checkout_Payment extends lC_Template {
-
-    /* Private variables */
-    var $_module = 'payment',
-        $_group = 'checkout',
-        $_page_title,
-        $_page_contents = 'checkout_payment.php',
-        $_page_image = 'table_background_payment.gif';
-
-    /* Class constructor */
-    function lC_Checkout_Payment() {
-      global $lC_Database, $lC_Session, $lC_ShoppingCart, $lC_Customer, $lC_Services, $lC_Language, $lC_NavigationHistory, $lC_Breadcrumb, $lC_Payment;
-
-      if ($lC_Customer->isLoggedOn() === false) {
-        $lC_NavigationHistory->setSnapshot();
-
-        lc_redirect(lc_href_link(FILENAME_ACCOUNT, 'login', 'SSL'));
-      }
-
-      if ($lC_ShoppingCart->hasContents() === false) {
-        lc_redirect(lc_href_link(FILENAME_CHECKOUT, null, 'SSL'));
-      }
-
-      // if no shipping method has been selected, redirect the customer to the shipping method selection page
-      if ($lC_ShoppingCart->hasShippingMethod() === false) {
-        lc_redirect(lc_href_link(FILENAME_CHECKOUT, 'shipping', 'SSL'));
-      }
-
-      // Stock Check
-      if ( (STOCK_CHECK == '1') && (STOCK_ALLOW_CHECKOUT == '-1') ) {
-        foreach ($lC_ShoppingCart->getProducts() as $products) {
-          if ($lC_ShoppingCart->isInStock($products['item_id']) === false) {
-            lc_redirect(lc_href_link(FILENAME_CHECKOUT, 'SSL'));
-            break;
-          }
-        }
-      }
-
-      $this->_page_title = $lC_Language->get('payment_method_heading');
-
-      if ($lC_Services->isStarted('breadcrumb')) {
-        $lC_Breadcrumb->add($lC_Language->get('breadcrumb_checkout_payment'), lc_href_link(FILENAME_CHECKOUT, $this->_module, 'SSL'));
-      }
-
-      // redirect to the billing address page when no default address exists
-      if ($lC_Customer->hasDefaultAddress() === false) {
-        $this->_page_title = $lC_Language->get('payment_address_heading');
-        $this->_page_contents = 'checkout_payment_address.php';
-
-        $this->addJavascriptFilename('templates/' . $this->getCode() . '/javascript/checkout_payment_address.js');
-        $this->addJavascriptPhpFilename('includes/form_check.js.php');
-      } else {
-        $this->addJavascriptFilename('templates/' . $this->getCode() . '/javascript/checkout_payment.js');
-
-        // if no billing destination address was selected, use the customers own address as default
-        if ($lC_ShoppingCart->hasBillingAddress() == false) {
-          $lC_ShoppingCart->setBillingAddress($lC_Customer->getDefaultAddressID());
-        } else {
-          // verify the selected billing address
-          $Qcheck = $lC_Database->query('select address_book_id from :table_address_book where address_book_id = :address_book_id and customers_id = :customers_id limit 1');
-          $Qcheck->bindTable(':table_address_book', TABLE_ADDRESS_BOOK);
-          $Qcheck->bindInt(':address_book_id', $lC_ShoppingCart->getBillingAddress('id'));
-          $Qcheck->bindInt(':customers_id', $lC_Customer->getID());
-          $Qcheck->execute();
-
-          if ($Qcheck->numberOfRows() !== 1) {
-            $lC_ShoppingCart->setBillingAddress($lC_Customer->getDefaultAddressID());
-            $lC_ShoppingCart->resetBillingMethod();
-          }
-        }
-
-        // load all enabled payment modules
-        include('includes/classes/payment.php');
-        $lC_Payment = new lC_Payment();
-
-        $this->addJavascriptBlock($lC_Payment->getJavascriptBlocks());
-      }
-
-      if (isset($_GET['payment_error']) && is_object(${$_GET['payment_error']}) && ($error = ${$_GET['payment_error']}->get_error())) {
-        $lC_MessageStack->add('checkout_payment', $error['error'], 'error');
-      }
-    }
-  }
-?>
+<?php
+/*
+  $Id: payment.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+
+  require('includes/classes/address_book.php');
+
+  class lC_Checkout_Payment extends lC_Template {
+
+    /* Private variables */
+    var $_module = 'payment',
+        $_group = 'checkout',
+        $_page_title,
+        $_page_contents = 'checkout_payment.php',
+        $_page_image = 'table_background_payment.gif';
+
+    /* Class constructor */
+    function lC_Checkout_Payment() {
+      global $lC_Database, $lC_Session, $lC_ShoppingCart, $lC_Customer, $lC_Services, $lC_Language, $lC_NavigationHistory, $lC_Breadcrumb, $lC_Payment;
+
+      if ($lC_Customer->isLoggedOn() === false) {
+        $lC_NavigationHistory->setSnapshot();
+
+        lc_redirect(lc_href_link(FILENAME_ACCOUNT, 'login', 'SSL'));
+      }
+
+      if ($lC_ShoppingCart->hasContents() === false) {
+        lc_redirect(lc_href_link(FILENAME_CHECKOUT, null, 'SSL'));
+      }
+
+      // if no shipping method has been selected, redirect the customer to the shipping method selection page
+      if ($lC_ShoppingCart->hasShippingMethod() === false) {
+        lc_redirect(lc_href_link(FILENAME_CHECKOUT, 'shipping', 'SSL'));
+      }
+
+      // Stock Check
+      if ( (STOCK_CHECK == '1') && (STOCK_ALLOW_CHECKOUT == '-1') ) {
+        foreach ($lC_ShoppingCart->getProducts() as $products) {
+          if ($lC_ShoppingCart->isInStock($products['item_id']) === false) {
+            lc_redirect(lc_href_link(FILENAME_CHECKOUT, 'SSL'));
+            break;
+          }
+        }
+      }
+
+      $this->_page_title = $lC_Language->get('payment_method_heading');
+
+      if ($lC_Services->isStarted('breadcrumb')) {
+        $lC_Breadcrumb->add($lC_Language->get('breadcrumb_checkout_payment'), lc_href_link(FILENAME_CHECKOUT, $this->_module, 'SSL'));
+      }
+
+      // redirect to the billing address page when no default address exists
+      if ($lC_Customer->hasDefaultAddress() === false) {
+        $this->_page_title = $lC_Language->get('payment_address_heading');
+        $this->_page_contents = 'checkout_payment_address.php';
+
+        $this->addJavascriptFilename('templates/' . $this->getCode() . '/javascript/checkout_payment_address.js');
+        $this->addJavascriptPhpFilename('includes/form_check.js.php');
+      } else {
+        $this->addJavascriptFilename('templates/' . $this->getCode() . '/javascript/checkout_payment.js');
+
+        // if no billing destination address was selected, use the customers own address as default
+        if ($lC_ShoppingCart->hasBillingAddress() == false) {
+          $lC_ShoppingCart->setBillingAddress($lC_Customer->getDefaultAddressID());
+        } else {
+          // verify the selected billing address
+          $Qcheck = $lC_Database->query('select address_book_id from :table_address_book where address_book_id = :address_book_id and customers_id = :customers_id limit 1');
+          $Qcheck->bindTable(':table_address_book', TABLE_ADDRESS_BOOK);
+          $Qcheck->bindInt(':address_book_id', $lC_ShoppingCart->getBillingAddress('id'));
+          $Qcheck->bindInt(':customers_id', $lC_Customer->getID());
+          $Qcheck->execute();
+
+          if ($Qcheck->numberOfRows() !== 1) {
+            $lC_ShoppingCart->setBillingAddress($lC_Customer->getDefaultAddressID());
+            $lC_ShoppingCart->resetBillingMethod();
+          }
+        }
+
+        // load all enabled payment modules
+        include('includes/classes/payment.php');
+        $lC_Payment = new lC_Payment();
+
+        $this->addJavascriptBlock($lC_Payment->getJavascriptBlocks());
+      }
+
+      if (isset($_GET['payment_error']) && is_object(${$_GET['payment_error']}) && ($error = ${$_GET['payment_error']}->get_error())) {
+        $lC_MessageStack->add('checkout_payment', $error['error'], 'error');
+      }
+    }
+  }
+?>
