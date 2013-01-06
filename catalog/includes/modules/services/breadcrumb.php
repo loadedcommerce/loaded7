6c6ee3c95fc4eb3d3122f0994cb374467d41c691
datazen <scott.logsdon@loadedcommerce.com>
2013-01-06 10:48:58 -0500

    [IC] removed whitespace

--- catalog/includes/modules/services/breadcrumb.php
+++ catalog/includes/modules/services/breadcrumb.php
@@ -1,32 +1,32 @@
-<?php
-/*
-  $Id: breadcrunb.php v1.0 2013-01-01 datazen $
-
-  LoadedCommerce, Innovative eCommerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2013 Loaded Commerce, LLC
-
-  @author     LoadedCommerce Team
-  @copyright  (c) 2013 LoadedCommerce Team
-  @license    http://loadedcommerce.com/license.html
-*/
-class lC_Services_breadcrumb {
-
-  public function start() {
-    global $lC_Breadcrumb, $lC_Language;
-
-    include('includes/classes/breadcrumb.php');
-    $lC_Breadcrumb = new lC_Breadcrumb();
-
-    //$lC_Breadcrumb->add($lC_Language->get('breadcrumb_top'), HTTP_SERVER);
-    $lC_Breadcrumb->add('<span id="breadcrumbImg">' . lc_image(DIR_WS_CATALOG . 'templates/' . $_SESSION['template']['code'] . '/images/iconHome.png'), lc_href_link(FILENAME_DEFAULT)) . '</span>';
-
-    return true;
-  }
-
-  public function stop() {
-    return true;
-  }
-}
+<?php
+/*
+  $Id: breadcrunb.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+class lC_Services_breadcrumb {
+
+  public function start() {
+    global $lC_Breadcrumb, $lC_Language;
+
+    include('includes/classes/breadcrumb.php');
+    $lC_Breadcrumb = new lC_Breadcrumb();
+
+    //$lC_Breadcrumb->add($lC_Language->get('breadcrumb_top'), HTTP_SERVER);
+    $lC_Breadcrumb->add('<span id="breadcrumbImg">' . lc_image(DIR_WS_CATALOG . 'templates/' . $_SESSION['template']['code'] . '/images/iconHome.png'), lc_href_link(FILENAME_DEFAULT)) . '</span>';
+
+    return true;
+  }
+
+  public function stop() {
+    return true;
+  }
+}
 ?>
--- catalog/includes/modules/services/reviews.php
+++ catalog/includes/modules/services/reviews.php
@@ -1,28 +1,28 @@
-<?php
-/*
-  $Id$
-
-  LoadedCommerce, Open Source E-Commerce Solutions
-  http://www.loadedcommerce.com
-
-  Copyright (c) 2007 LoadedCommerce
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License v2 (1991)
-  as published by the Free Software Foundation.
-*/
-
-  class lC_Services_reviews {
-    function start() {
-    	global $lC_Reviews;
-      include('includes/classes/reviews.php');
-
-      $lC_Reviews = new lC_Reviews();
-      return true;
-    }
-
-    function stop() {
-      return true;
-    }
-  }
-?>
+<?php
+/*
+  $Id$
+
+  LoadedCommerce, Open Source E-Commerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2007 LoadedCommerce
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License v2 (1991)
+  as published by the Free Software Foundation.
+*/
+
+  class lC_Services_reviews {
+    function start() {
+    	global $lC_Reviews;
+      include('includes/classes/reviews.php');
+
+      $lC_Reviews = new lC_Reviews();
+      return true;
+    }
+
+    function stop() {
+      return true;
+    }
+  }
+?>
