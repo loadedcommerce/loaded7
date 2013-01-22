fbe903efc71b7aed33c8790cbd84e380ec30a361
datazen <scott.logsdon@loadedcommerce.com>
2013-01-22 12:01:22 -0500

    id:686 - working on address book details page

--- catalog/templates/default/content/account/address_book_process.php
+++ catalog/templates/default/content/account/address_book_process.php
@@ -1,5 +1,5 @@
 <?php
-  /*
+/*
   $Id: address_book_process.php v1.0 2013-01-01 datazen $
 
   LoadedCommerce, Innovative eCommerce Solutions
@@ -10,17 +10,17 @@
   @author     LoadedCommerce Team
   @copyright  (c) 2013 LoadedCommerce Team
   @license    http://loadedcommerce.com/license.html
-  */
-  if (isset($_GET['edit'])) {
-    $Qentry = lC_AddressBook::getEntry($_GET['address_book']);
-  } else {
-    if (lC_AddressBook::numberOfEntries() >= MAX_ADDRESS_BOOK_ENTRIES) {
-      $lC_MessageStack->add('address_book', $lC_Language->get('error_address_book_full'));
-    }
+*/
+if (isset($_GET['edit'])) {
+  $Qentry = lC_AddressBook::getEntry($_GET['address_book']);
+} else {
+  if (lC_AddressBook::numberOfEntries() >= MAX_ADDRESS_BOOK_ENTRIES) {
+    $lC_MessageStack->add('address_book', $lC_Language->get('error_address_book_full'));
   }
-  if ($lC_MessageStack->size('address_book') > 0) {
-    echo '<br /><div class="short-code msg error"><span>' . $lC_MessageStack->get('address_book', DIR_WS_TEMAPLTE_IMAGES . 'shortcodes/', '.png') . '</span></div>';
-  }
+}
+if ($lC_MessageStack->size('address_book') > 0) {
+  echo '<br /><div class="short-code msg error"><span>' . $lC_MessageStack->get('address_book', DIR_WS_TEMAPLTE_IMAGES . 'shortcodes/', '.png') . '</span></div>';
+}
 ?>
 <!--ADDRESS BOOK PROCESS SECTION STARTS-->
 <div id="addressBookProcess" class="full_page">
@@ -28,17 +28,17 @@
     <div class="short-code-column">
       <h1><?php echo $lC_Template->getPageTitle(); ?></h1>
       <?php
-      if ( ($lC_Customer->hasDefaultAddress() === false) || (isset($_GET['new']) && (lC_AddressBook::numberOfEntries() < MAX_ADDRESS_BOOK_ENTRIES)) || (isset($Qentry) && ($Qentry->numberOfRows() === 1)) ) {
+        if ( ($lC_Customer->hasDefaultAddress() === false) || (isset($_GET['new']) && (lC_AddressBook::numberOfEntries() < MAX_ADDRESS_BOOK_ENTRIES)) || (isset($Qentry) && ($Qentry->numberOfRows() === 1)) ) {
         ?>
         <form name="address_book" id="address_book" action="<?php echo lc_href_link(FILENAME_ACCOUNT, 'address_book=' . $_GET['address_book'] . '&' . (isset($_GET['edit']) ? 'edit' : 'new') . '=save', 'SSL'); ?>" method="post">
           <?php
-          if (file_exists(DIR_FS_TEMPLATE . 'modules/address_book_details.php')) {
-            require(DIR_FS_TEMPLATE . 'modules/address_book_details.php');
-          } else {
-            require('includes/modules/address_book_details.php');
-          }
+            if (file_exists(DIR_FS_TEMPLATE . 'modules/address_book_details.php')) {
+              require(DIR_FS_TEMPLATE . 'modules/address_book_details.php');
+            } else {
+              require('includes/modules/address_book_details.php');
+            }
           ?>
-          
+
           <div style="clear:both;">&nbsp;</div>
           <!--ADDRESS BOOK PROCESS ACTIONS STARTS-->       
           <div id="addressBookProcessActions" class="action_buttonbar">
@@ -52,13 +52,13 @@
               }
             ?>
             <span class="buttonLeft"><a href="<?php echo $back_link; ?>" class="noDecoration"><button class="button brown_btn" type="button"><?php echo $lC_Language->get('button_back'); ?></button></a></span>
-            <span class="buttonRight"><button class="button purple_btn" type="submit"><?php echo $lC_Language->get('button_continue'); ?></button></span>
+            <span class="buttonRight"><button class="button purple_btn" type="submit"><?php echo $lC_Language->get('button_update'); ?></button></span>
             <div style="clear:both;"></div> 
           </div>
           <!--ADDRESS BOOK PROCESS ACTIONS ENDS-->
         </form>
         <?php
-      } else {
+        } else {
         ?>
         <!--ADDRESS BOOK PROCESS ACTIONS STARTS-->
         <div id="addressBookProcessActions" class="action_buttonbar">
@@ -67,10 +67,10 @@
         <div style="clear:both;"></div>
         <!--ADDRESS BOOK PROCESS ACTIONS ENDS--> 
         <?php
-      }
+        }
       ?>
     </div>
   </div>
   <!--ADDRESS BOOK PROCESS CONTENT ENDS-->
-  </div>
+</div>
 <!--ADDRESS BOOK PROCESS SECTION ENDS-->
