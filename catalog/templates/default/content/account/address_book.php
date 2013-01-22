7578af20b3dd7bce8ef3b749a9f6b5b852ef1000
datazen <scott.logsdon@loadedcommerce.com>
2013-01-22 13:46:22 -0500

    id:686 - working on address book details page

--- catalog/templates/default/content/account/address_book.php
+++ catalog/templates/default/content/account/address_book.php
@@ -16,78 +16,78 @@ if ($lC_MessageStack->size('address_book') > 0) {
 }
 ?>
 <!--ADDRESS BOOK SECTION STARTS-->
-  <div class="full_page">
-    <!--ADDRESS BOOK CONTENT STARTS-->
-    <div class="content">
-      <div class="short-code-column">
-        <h1><?php echo $lC_Template->getPageTitle(); ?></h1>
-        <p><b><?php echo $lC_Language->get('primary_address_title'); ?></b></p>
-        <div class="borderPadMe">  
-          <div>
-            <div style="float: right; padding: 0px 0px 10px 20px;">
-              <?php echo lC_Address::format($lC_Customer->getDefaultAddressID(), '<br />'); ?>
-            </div>
-            <div style="float: right; padding: 0px 0px 10px 20px; text-align: center;">
-              <?php echo '<b>' . $lC_Language->get('primary_address_title') . '</b><br />' . lc_image(DIR_WS_TEMPLATE_IMAGES . 'arrow_south_east.png'); ?>
-            </div>
-            <?php echo $lC_Language->get('primary_address_description'); ?>
-            <div style="clear: both;"></div>
+<div class="full_page">
+  <!--ADDRESS BOOK CONTENT STARTS-->
+  <div class="content">
+    <div class="short-code-column">
+      <h1><?php echo $lC_Template->getPageTitle(); ?></h1>
+      <p><b><?php echo $lC_Language->get('primary_address_title'); ?></b></p>
+      <div class="borderPadMe">  
+        <div>
+          <div style="float: right; padding: 0px 0px 10px 20px;">
+            <?php echo lC_Address::format($lC_Customer->getDefaultAddressID(), '<br />'); ?>
           </div>
+          <div style="float: right; padding: 0px 0px 10px 20px; text-align: center;">
+            <?php echo '<b>' . $lC_Language->get('primary_address_title') . '</b><br />' . lc_image(DIR_WS_TEMPLATE_IMAGES . 'arrow_south_east.png'); ?>
+          </div>
+          <?php echo $lC_Language->get('primary_address_description'); ?>
+          <div style="clear: both;"></div>
         </div>
-        <div style="clear: both;">&nbsp;</div>
-        <p><b><?php echo $lC_Language->get('address_book_title'); ?></b></p>
-        <div class="borderPadMe">
-          <table border="0" width="100%" cellspacing="0" cellpadding="2">
-          <?php
-            $Qaddresses = lC_AddressBook::getListing();
-            while ($Qaddresses->next()) {
-            ?>
-            <tr>
-              <td style="vertical-align:top;">
-                <b><?php echo $Qaddresses->valueProtected('firstname') . ' ' . $Qaddresses->valueProtected('lastname'); ?></b>
-                <?php
-                  if ($Qaddresses->valueInt('address_book_id') == $lC_Customer->getDefaultAddressID()) {
-                    echo '&nbsp;<small><i>' . $lC_Language->get('primary_address_marker') . '</i></small>';
-                  }
-                ?>
-              </td>
-              <td>
-              <?php 
-                echo '<span class="buttonRight">' . 
-                     '  <a href="' . lc_href_link(FILENAME_ACCOUNT, 'address_book=' . $Qaddresses->valueInt('address_book_id') . '&edit', 'SSL')  . '" style="text-decoration:none;">' . 
-                     '    <button class="button brown_btn" type="button">' . $lC_Language->get('button_edit') . '</button>' . 
-                     '  </a>' .
-                     '  <a href="' . lc_href_link(FILENAME_ACCOUNT, 'address_book=' . $Qaddresses->valueInt('address_book_id') . '&delete', 'SSL')  . '" style="text-decoration:none;">' . 
-                     '    <button class="button brown_btn" type="button">' . $lC_Language->get('button_delete') . '</button>' . 
-                     '  </a>' . 
-                     '</span>';
-              ?>
-              </td>
-            </tr>
-            <tr>
-              <td colspan="2" style="padding: 0px 0px 10px 10px;"><?php echo lC_Address::format($Qaddresses->toArray(), '<br />'); ?></td>
-            </tr>
-            <?php
-            }
-          ?>
-          </table>
-        </div>
-        <div style="clear: both;">&nbsp;</div>
-        <div id ="addressBookActions" class="action_buttonbar">
-          <span class="buttonLeft"><a href="<?php echo lc_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>" class="noDecoration"><button class="button brown_btn" type="button"><?php echo $lC_Language->get('button_back'); ?></button></a></span>
-          <?php
-            if ($Qaddresses->numberOfRows() < MAX_ADDRESS_BOOK_ENTRIES) {
-          ?>
-          <span class="buttonRight"><a href="<?php echo lc_href_link(FILENAME_ACCOUNT, 'address_book&new', 'SSL'); ?>" class="noDecoration"><button class="button brown_btn" type="button"><?php echo $lC_Language->get('button_add_address'); ?></button></a></span>
-          <?php
-            } else {
-              echo sprintf($lC_Language->get('address_book_maximum_entries'), MAX_ADDRESS_BOOK_ENTRIES);
-            }
-          ?>
-        </div> 
-        <div style="clear:both;"></div>
       </div>
+      <div style="clear: both;">&nbsp;</div>
+      <p><b><?php echo $lC_Language->get('address_book_title'); ?></b></p>
+      <div class="borderPadMe">
+        <table border="0" width="100%" cellspacing="0" cellpadding="2">
+        <?php
+          $Qaddresses = lC_AddressBook::getListing();
+          while ($Qaddresses->next()) {
+          ?>
+          <tr>
+            <td style="vertical-align:top;">
+              <b><?php echo $Qaddresses->valueProtected('firstname') . ' ' . $Qaddresses->valueProtected('lastname'); ?></b>
+              <?php
+                if ($Qaddresses->valueInt('address_book_id') == $lC_Customer->getDefaultAddressID()) {
+                  echo '&nbsp;<small><i>' . $lC_Language->get('primary_address_marker') . '</i></small>';
+                }
+              ?>
+            </td>
+            <td>
+            <?php 
+              echo '<span class="buttonRight">' . 
+                   '  <a href="' . lc_href_link(FILENAME_ACCOUNT, 'address_book=' . $Qaddresses->valueInt('address_book_id') . '&edit', 'SSL')  . '" style="text-decoration:none;">' . 
+                   '    <button class="button brown_btn" type="button">' . $lC_Language->get('button_edit') . '</button>' . 
+                   '  </a>' .
+                   '  <a href="' . lc_href_link(FILENAME_ACCOUNT, 'address_book=' . $Qaddresses->valueInt('address_book_id') . '&delete', 'SSL')  . '" style="text-decoration:none;">' . 
+                   '    <button class="button brown_btn" type="button">' . $lC_Language->get('button_delete') . '</button>' . 
+                   '  </a>' . 
+                   '</span>';
+            ?>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2" style="padding: 0px 0px 10px 10px;"><?php echo lC_Address::format($Qaddresses->toArray(), '<br />'); ?></td>
+          </tr>
+          <?php
+          }
+        ?>
+        </table>
+      </div>
+      <div style="clear: both;">&nbsp;</div>
+      <div id ="addressBookActions" class="action_buttonbar">
+        <span class="buttonLeft"><a href="<?php echo lc_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>" class="noDecoration"><button class="button brown_btn" type="button"><?php echo $lC_Language->get('button_back'); ?></button></a></span>
+        <?php
+          if ($Qaddresses->numberOfRows() < MAX_ADDRESS_BOOK_ENTRIES) {
+        ?>
+        <span class="buttonRight"><a href="<?php echo lc_href_link(FILENAME_ACCOUNT, 'address_book&new', 'SSL'); ?>" class="noDecoration"><button class="button brown_btn" type="button"><?php echo $lC_Language->get('button_add_address'); ?></button></a></span>
+        <?php
+          } else {
+            echo sprintf($lC_Language->get('address_book_maximum_entries'), MAX_ADDRESS_BOOK_ENTRIES);
+          }
+        ?>
+      </div> 
+      <div style="clear:both;"></div>
     </div>
-    <!--ADDRESS BOOK CONTENT STARTS-->
   </div>
+  <!--ADDRESS BOOK CONTENT STARTS-->
+</div>
 <!--ADDRESS BOOK CONTENT STARTS-->
