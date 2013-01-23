f942ae057113b58493ccdeb3f2b1adc949149f7d
geraldbullard <gerald.bullard@loadedcommerce.com>
2013-01-23 17:43:10 -0500

    [IC] added addressBookDetails.js.php to templates javascript folder to handle two main functions of country and zones for address_book_details.php previously in the page specific js.php files. now the page specific js.php files only have the jQuery form validation. The core controller files now make the addition to the js array to bring in the two functions added to addressBookDetails.js.php. also the lc_href_link json call was updated to AUTO for any env ajax situation

--- catalog/templates/default/javascript/account/address_book.js.php
+++ catalog/templates/default/javascript/account/address_book.js.php
@@ -13,62 +13,33 @@
  
   @function The lC_Default class manages default template functions
 */ 
-global $lC_Language, $Qentry; 
-
-if (isset($_GET['address_book']) && is_numeric($_GET['address_book'])) {
-  ?>
-  <script>
-  $(document).ready(function() {
-    var country = '<?php echo (isset($Qentry) ? $Qentry->valueInt('entry_country_id') : STORE_COUNTRY); ?>';
-    var zone = '<?php echo (isset($Qentry) ? $Qentry->value('entry_zone_id') : null); ?>'; 
-    var zoneName = '<?php echo (isset($Qentry) ? $Qentry->value('entry_state') : null); ?>'; 
-    if (zone == '0') zone = zoneName;
-    getZonesDropdown(country, zone);  
-  });
-
-  $('#address_book').submit(function() {
-    var fnameMin = '<?php echo ACCOUNT_FIRST_NAME; ?>';
-    var lnameMin = '<?php echo ACCOUNT_LAST_NAME; ?>';
-    jQuery.validator.messages.required = "";
-    var bValid = $("#address_book").validate({
-      rules: {
-        firstname: { minlength: fnameMin, required: true },
-        lastname: { minlength: lnameMin, required: true },
-        street_address: { required: true },
-        city: { required: true },
-      },
-      invalidHandler: function(e, validator) {
-        var errors = validator.numberOfInvalids();
-        if (errors) {
-          $("#errDiv").show().delay(5000).fadeOut('slow');
-        } else {
-          $("#errDiv").hide();
-        }
-        return false;
-      }
-    }).form();
-
-    if (bValid) {      
-      $('#address_book').submit();
-    }
-    return false;
-  });
-
-  function getZonesDropdown(country, zone) {
-    var jsonLink = '<?php echo lc_href_link('rpc.php', 'action=getZonesDropdown&country=COUNTRY&zone=ZONE'); ?>';   
-    $.getJSON(jsonLink.replace('COUNTRY', country).replace('&amp;', '&').replace('ZONE', zone).replace('&amp;', '&'),
-      function (data) {
-        if (data.rpcStatus != 1) {
-          alert('<?php echo $lC_Language->get('ms_error_action_not_performed'); ?>');
-          return false;
-        }
-        $('#uniform-zones').html(data.zonesHtml).change();
-        if ($.browser.mozilla) $('#uniform-zones select').attr( "style", "padding-top:6px" );
-        if (data.single == '1') $('#uniform-zones').attr('style', 'padding:0 0 5px 0;');
-      }
-    );
-  }
-  </script>
-  <?php
-}
 ?>
+<script>
+$('#address_book').submit(function() {
+  var fnameMin = '<?php echo ACCOUNT_FIRST_NAME; ?>';
+  var lnameMin = '<?php echo ACCOUNT_LAST_NAME; ?>';
+  jQuery.validator.messages.required = "";
+  var bValid = $("#address_book").validate({
+    rules: {
+      firstname: { minlength: fnameMin, required: true },
+      lastname: { minlength: lnameMin, required: true },
+      street_address: { required: true },
+      city: { required: true },
+    },
+    invalidHandler: function(e, validator) {
+      var errors = validator.numberOfInvalids();
+      if (errors) {
+        $("#errDiv").show().delay(5000).fadeOut('slow');
+      } else {
+        $("#errDiv").hide();
+      }
+      return false;
+    }
+  }).form();
+
+  if (bValid) {      
+    $('#address_book').submit();
+  }
+  return false;
+});
+</script>
