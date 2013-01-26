5f4f7370224c782010a307c7a09c210a8ce80153
datazen <scott.logsdon@loadedcommerce.com>
2013-01-25 22:05:11 -0500

    [IC] manual merge

--- catalog/templates/default/javascript/checkout/shipping.js.php
+++ catalog/templates/default/javascript/checkout/shipping.js.php
@@ -1,20 +1,28 @@
 <script>
-  $('#shippingSelect tr').click(function() {
-    $(this).find('td input:radio').prop('checked', true);
-  });
-  var selected;
+  //$('#shippingSelect tr').click(function() {
+  //  $(this).find('td input:radio').prop('checked', true);
+  //});
+  
+var selected;
 
-  function selectRowEffect(object, buttonSelect) {
-    if (!selected) {
-      if (document.getElementById) {
-        selected = document.getElementById('defaultSelected');
-      } else {
-        selected = document.all['defaultSelected'];
-      }
+function selectRowEffect(object, buttonSelect) {
+  if (!selected) {
+    if (document.getElementById) {
+      selected = document.getElementById('defaultSelected');
+    } else {
+      selected = document.all['defaultSelected'];
     }
-
-    if (selected) selected.className = 'moduleRow';
-    object.className = 'moduleRowSelected';
-    selected = object;
   }
+
+  if (selected) selected.className = 'moduleRow';
+  object.className = 'moduleRowSelected';
+  selected = object;
+
+// one button is not an array
+  if (document.checkout_address.shipping[0]) {
+    document.checkout_address.shipping[buttonSelect].checked=true;
+  } else {
+    document.checkout_address.shipping.checked=true;
+  }
+}
 </script>
