4d6813c9dfb908abf9ab02fd75f87a6ed97ffb4d
geraldbullard <gerald.bullard@loadedcommerce.com>
2013-01-25 20:03:06 -0500

    [IC] removed un needed code from javascript

--- catalog/templates/default/javascript/checkout/shipping.js.php
+++ catalog/templates/default/javascript/checkout/shipping.js.php
@@ -16,12 +16,5 @@
     if (selected) selected.className = 'moduleRow';
     object.className = 'moduleRowSelected';
     selected = object;
-
-    // one button is not an array
-    if (document.checkout_address.shipping[0]) {
-      document.checkout_address.shipping[buttonSelect].checked=true;
-    } else {
-      document.checkout_address.shipping.checked=true;
-    }
   }
 </script>
