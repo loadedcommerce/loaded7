3991b3cc55fb8d15529bf361341400fa11b73574
wa4u <kiran@thereactorworks.com>
2013-02-08 02:31:02 +0530

    [D728] Contact Us Page Layout
    Updated contact us page layout and stayles to match template styles.
    Added validations.

--- catalog/templates/default/content/info/info_contact.php
+++ catalog/templates/default/content/info/info_contact.php
@@ -1,15 +1,15 @@
 <?php
-/**  
-*  $Id: info_contact.php v1.0 2013-01-01 datazen $
-*
-*  LoadedCommerce, Innovative eCommerce Solutions
-*  http://www.loadedcommerce.com
-*
-*  Copyright (c) 2013 Loaded Commerce, LLC
-*
-*  @author     Loaded Commerce Team
-*  @copyright  (c) 2013 Loaded Commerce Team
-*  @license    http://loadedcommerce.com/license.html
+/*
+  $Id: info_contact.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
 */
 if ($lC_MessageStack->size('contact') > 0) {
   echo '<br /><div class="short-code msg error"><span>' . $lC_MessageStack->get('contact', DIR_WS_TEMAPLTE_IMAGES . 'shortcodes/', '.png') . '</span></div>';
@@ -17,34 +17,46 @@ if ($lC_MessageStack->size('contact') > 0) {
 ?>
 <!--content/info/info_contact.php start-->
 <div id="infoContact" class="full_page">
-  <h1><?php echo $lC_Template->getPageTitle(); ?></h1>        
- 	<div class="content">
-    <div id="infoContactInner">
-      <form name="contact" action="<?php echo lc_href_link(FILENAME_INFO, 'contact=process'); ?>" method="post" enctype="multipart/form-data" id="contact">
-      <?php 
-      if (isset($_GET['contact']) && ($_GET['contact'] == 'success')) {
-        echo $lC_Language->get('contact_email_sent_successfully');
-      } else {
-        ?>
-        <div id="infoContactForm">
-          <div class="short-code-column">
-            <?php echo $lC_Language->get('contact_store_address_title') . ':<br />' . nl2br(STORE_NAME_ADDRESS); ?><br /><br />
-            <label for="name"><?php echo $lC_Language->get('contact_name_title') . '</label> ' . lc_draw_input_field('name', null, ''); ?><br />
-            <label for="email"><?php echo  $lC_Language->get('contact_email_address_title') . '</label> ' . lc_draw_input_field('email', null, ''); ?><br />
-            <label for="inquiry"><?php echo $lC_Language->get('contact_inquiry_title') . '</label> ' . lc_draw_textarea_field('inquiry', null, 40, 8, 'style="width:50%;"'); ?><br />
-          </div> 
+  <form name="contact" action="<?php echo lc_href_link(FILENAME_INFO, 'contact=process'); ?>" method="post" enctype="multipart/form-data" id="contact">
+    <div class="short-code-column">
+      <h1><?php echo $lC_Template->getPageTitle(); ?></h1>
+      <div id="errDiv" class="short-code msg error" style="margin-bottom:10px; display:none;"> <span><?php echo $lC_Language->get('form_validation_error'); ?></span> </div>
+      <div class="single-bg">
+        <div class="embed-form short-code-column margin-bottom">
+          <h3><?php echo $lC_Language->get('contact_store_address_title');?></h3>
+          <?php echo nl2br(STORE_NAME_ADDRESS); ?>
         </div>
+      </div>
+      <div class="single-bg">
+        <div class="embed-form short-code-column margin-bottom">
+        <?php 
+        $contact_name = '';
+        $contact_email = '';
+        $contact_button = '<a onclick="$(\'#contact\').submit();" class="noDecoration"><button class="button brown_btn" type="submit">' . $lC_Language->get('button_continue') . '</button></a>';
+        if (isset($_GET['contact']) && ($_GET['contact'] == 'success')) {
+            echo $lC_Language->get('contact_email_sent_successfully');
+            $contact_button = '<a href="' . lc_href_link(FILENAME_DEFAULT) . '" class="noDecoration"><button class="button brown_btn" type="button">' . $lC_Language->get('button_continue') . '</button></a>';
+        } else {
+            if (isset($_SESSION['lC_Customer_data'])) {
+                $contact_name = $_SESSION['lC_Customer_data']['first_name'] . ' ' . $_SESSION['lC_Customer_data']['last_name'];
+                $contact_email = $_SESSION['lC_Customer_data']['email_address'];
+            }
+        ?>
+          <ul id="personal_details">
+            <li><?php echo lc_draw_label('', 'name', null, false) . ' ' . lc_draw_input_field('name', $contact_name , 'placeholder="' . $lC_Language->get('contact_name_title') . '" onfocus="this.placeholder = \'\'" onblur="this.placeholder = \'' . $lC_Language->get('contact_name_title') . '\'" holder="' . $lC_Language->get('contact_name_title') . '" class="txt" style="width:99%;"'); ?></li>
+            <li><?php echo lc_draw_label('', 'email', null, '', false) . ' ' . lc_draw_input_field('email', $contact_email, 'placeholder="' . $lC_Language->get('contact_email_address_title') . '" onfocus="this.placeholder = \'\'" onblur="this.placeholder = \'' . $lC_Language->get('contact_email_address_title') . '\'" holder="' . $lC_Language->get('contact_email_address_title') . '" class="txt" style="width:99%;"');?></li>
+            <li><?php echo lc_draw_label('', 'inquiry', null, false) . ' ' . lc_draw_textarea_field('inquiry', null, 40, 8, 'placeholder="' . $lC_Language->get('contact_inquiry_title') . '" onfocus="this.placeholder = \'\'" onblur="this.placeholder = \'' . $lC_Language->get('contact_inquiry_title') . '\'" holder="' . $lC_Language->get('contact_inquiry_title') . '"  holder="text" class="txt" style="width:99%;"'); ?></li>
+            <!--li>Place for Captcha</li -->
+          </ul>
         <?php
-      }
-      ?>
+        }
+        ?>
+        </div>
+      </div>
+      <div style="clear:both;"></div>
+      <div id="accountEditActions" class="action_buttonbar"  style="margin-top:10px;"><span class="buttonRight"><?php echo $contact_button;?></span></div>
+      <div style="clear:both;"></div>
     </div>
-    <div style="clear:both;">&nbsp;</div>
-    <div id="infoContactActions" class="action_buttonbar">
-      <span class="buttonLeft"><a href="<?php echo lc_href_link(FILENAME_INFO); ?>"><button class="button brown_btn" type="button"><?php echo $lC_Language->get('button_back'); ?></button></a></span>
-      <span class="buttonRight"><a onclick="$('#contact').submit();"><button class="button brown_btn" type="button"><?php echo $lC_Language->get('button_continue'); ?></button></a></span>
-    </div>
-    <div style="clear:both;"></div>
-    </form>
-  </div>
+  </form>
 </div>
 <!--content/info/info_contact.php end-->
