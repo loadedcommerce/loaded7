b4785dbee08f11be02cb48df9480d7ed81f1b388
datazen <scott.logsdon@loadedcommerce.com>
2013-02-07 17:36:10 -0500

    id:720 - working on code abstraction

--- catalog/templates/default/content/index/product_listing.php
+++ catalog/templates/default/content/index/product_listing.php
@@ -23,6 +23,7 @@ button.price_add {
     <!-- h1><?php echo $lC_Template->getPageTitle(); ?></h1 --> 
     <div class="products_list products_slider">
       <?php 
+      echo '[' . PRODUCT_LIST_FILTER . ']<br>';
       if (PRODUCT_LIST_FILTER == '1') echo lC_Default::getManufacturerFilter();
       
       $Qlisting = lC_Default::getProductsListingSql();
