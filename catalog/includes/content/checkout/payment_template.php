a331798f7ee2a10a134a4ba62d9dd56b0425e5c0
datazen <scott.logsdon@loadedcommerce.com>
2013-02-19 15:11:53 -0500

    id:643 - working on payment template page

--- catalog/includes/content/checkout/payment_template.php
+++ catalog/includes/content/checkout/payment_template.php
@@ -24,6 +24,8 @@ class lC_Checkout_Payment_template extends lC_Template {
       global $lC_Database, $lC_Session, $lC_ShoppingCart, $lC_Customer, $lC_Services, $lC_Language, $lC_NavigationHistory, $lC_Breadcrumb, $lC_Payment;
 
       $this->_page_title = $lC_Language->get('heading_title') . ' ' . session_name() . '=' . session_id();
+      
+      echo $this->_page_title;
   }
 }
 ?>
