45e55036d0999cc054b469e34858e1bef32d81ec
datazen <scott.logsdon@loadedcommerce.com>
2013-03-22 21:05:48 -0400

    [IC] general cleanup

--- catalog/admin/templates/default/general.js.php
+++ catalog/admin/templates/default/general.js.php
@@ -286,8 +286,18 @@ $(document).ready(function() {
     return false;
   });
   
+  // defeat Google Chrome form autofill and its yellow background
+  if(navigator.userAgent.toLowerCase().indexOf("chrome") >= 0 || navigator.userAgent.toLowerCase().indexOf("safari") >= 0){
+    window.setInterval(function(){
+      $('input:-webkit-autofill').each(function(){
+          var clone = $(this).clone(true, true);
+          $(this).after(clone).remove();
+      });
+    }, 20);
+  }
+  
   $("#profileLoader").hide();
-  $("#profileInner").fadeTo(1000, 1);
+  $("#profileInner").fadeTo(1000, 1);     
      
 });
 
