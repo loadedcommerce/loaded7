757c615101a189fad336d71258e87050e74fbd6a
datazen <scott.logsdon@loadedcommerce.com>
2013-03-27 08:41:19 -0400

    id:846 - adding missing info to secure payment page

--- catalog/templates/default/content/checkout/checkout_payment_template.php
+++ catalog/templates/default/content/checkout/checkout_payment_template.php
@@ -36,6 +36,14 @@
 @media only screen and (max-width: 320px) {
   #checkoutConfirmationDetails { width: 83% !important; } 
 }
+
+#paymentTemplateContainer .security-info-title { cursor:pointer; float:right; }
+#paymentTemplateContainer .security-info-text-container { margin:0 -5px 10px 0; background-color:#edfbec; }
+#paymentTemplateContainer .security-info-url { border:1px solid #ccc; padding:5px; }
+#paymentTemplateContainer .security-info-text { border:1px solid #ccc; padding:7px 5px; }
+#paymentTemplateContainer .arrow-container { margin-top:15px; }
+#paymentTemplateContainer .arrow-down { border-left: 10px solid transparent; border-right: 10px solid transparent; border-top: 10px solid gray; float: right; height: 0; width: 0; margin:13px 0 0 6px; }
+#paymentTemplateContainer .arrow-up { border-bottom: 10px solid gray; border-left: 10px solid transparent; border-right: 10px solid transparent; float: right; height: 0; width: 0; margin:13px 0 0 6px; } 
 </style>
 <div id="paymentTemplateContainer" class="full_page">
   <h5><?php echo $lC_Language->get('text_checkout'); ?></h5>
@@ -57,11 +65,21 @@
         <div class="step-title">
           <h2><?php echo $lC_Language->get('box_ordering_steps_secure_checkout'); ?></h2>
         </div>
-        
         <div id="checkout-step-login">
           <p><?php echo $lC_Language->get('secure_payment_description_text'); ?></p>
+          <script>
+            /* we use javascript here due to js scrubbing at payment endpoints - we don't want to show if javascript is disabled or scrubbed */
+            var output = '<div class="security-info-container">'+
+                         '<div class="security-info-title" onclick="toggleSecurityInfo();"><?php echo lc_image('images/greenlock.png', null, null, null, 'style="vertical-align:middle; margin:10px 5px;"') . $lC_Language->get('secure_payment_security_info_title'); ?><span class="arrow-container"><span id="arrow" class="arrow-down"></span></span></div>'+
+                         '<div style="clear:both;"></div>'+
+                         '<div class="security-info-text-container" style="display:none;">'+
+                         '  <div class="security-info-url"><?php echo lc_image('images/greenlock.png', null, null, null, 'style="vertical-align:middle; margin-right:5px;"'); ?>https://www.myurl.com</div>'+
+                         '  <div class="security-info-text"><?php echo $lC_Language->get('secure_payment_security_info_text'); ?></div>'+
+                         '</div></div><div style="clear:both;"></div>';
+            
+            document.write(output);            
+          </script>                   
           <div class="col2-set">
-          
             <div id="checkout_shipping_col1" style="width:32%; float:left;">
               <div id="ot-container">
                 <div class="ot-block" id="order-number">
