38e49a0e0c3e5ffd5982622c38e2171901627a7c
datazen <scott.logsdon@loadedcommerce.com>
2013-04-18 09:13:22 -0400

    [IC] repair file_put_contents warning

--- catalog/admin/external/vqmod/vqmod.php
+++ catalog/admin/external/vqmod/vqmod.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 /**
  * VQMod
  * @description Main Object used
@@ -413,7 +414,7 @@ class VQModLog {
       }
     }
 
-    $result = file_put_contents($logPath, implode(PHP_EOL, $txt), ($append ? FILE_APPEND : 0));
+    $result = @file_put_contents($logPath, implode(PHP_EOL, $txt), ($append ? FILE_APPEND : 0));
     if(!$result) {
       //die('LOG FILE COULD NOT BE WRITTEN');
     }
