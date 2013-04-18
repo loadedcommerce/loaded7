809d55d53462adc0eb8f90faaacb41fcbc0dfb90
datazen <scott.logsdon@loadedcommerce.com>
2013-04-18 09:39:02 -0400

    #760 - working on app store lister

---
+++ catalog/admin/includes/modules/access/store.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $Id: updates.php v1.0 2011-11-04 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2011 LoadedCommerce.com
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2011 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+
+  class lC_Access_Updates extends lC_Access {
+    var $_module = 'store',
+        $_group = 'hidden',
+        $_icon = '',
+        $_title,
+        $_sort_order = 100;
+
+    function lC_Access_Updates() {
+      global $lC_Language;
+
+      $this->_title = $lC_Language->get('access_store_title');
+    }
+  }
+?>
