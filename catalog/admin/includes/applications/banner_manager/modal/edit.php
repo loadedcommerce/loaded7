1e700ddc01a3e1995e7fb6c7bc60ab52960fe183
geraldbullard <gerald.bullard@loadedcommerce.com>
2013-04-25 13:43:02 -0400

    [B978] updated switch to hold/save correct value of banner status

--- catalog/admin/includes/applications/banner_manager/modal/edit.php
+++ catalog/admin/includes/applications/banner_manager/modal/edit.php
@@ -152,6 +152,7 @@ function editBanner(id) {
       if (data.bannerData.date_scheduled) $("#editDateScheduled").val(data.bannerData.date_scheduled.substr(0,10));
       if (data.bannerData.status == 1) { 
         $("#editStatus").attr('checked', true);
+        $("[tabindex=0]").addClass("checked");
       } else {
         $("#editStatus").attr('checked', false);
       } 
