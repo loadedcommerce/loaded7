24097ce2215a983c2b0262da15db9c11738221f0
geraldbullard <gerald.bullard@loadedcommerce.com>
2013-05-06 18:20:39 -0400

    more updates

--- catalog/admin/includes/applications/categories/pages/edit.php
+++ catalog/admin/includes/applications/categories/pages/edit.php
@@ -13,13 +13,20 @@
 */
 if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
   $lC_ObjectInfo = new lC_ObjectInfo(lC_Categories_Admin::get($_GET[$lC_Template->getModule()]));
-  $Qcd = $lC_Database->query('select categories_name, language_id from :table_categories_description where categories_id = :categories_id');
+  $Qcd = $lC_Database->query('select * from :table_categories_description where categories_id = :categories_id');
   $Qcd->bindTable(':table_categories_description', TABLE_CATEGORIES_DESCRIPTION);
   $Qcd->bindInt(':categories_id', $lC_ObjectInfo->get('categories_id'));
   $Qcd->execute();
   $categories_name = array();
   while ($Qcd->next()) {
     $categories_name[$Qcd->valueInt('language_id')] = $Qcd->value('categories_name');
+    $categories_menu_name[$Qcd->valueInt('language_id')] = $Qcd->value('categories_menu_name');
+    $categories_description[$Qcd->valueInt('language_id')] = $Qcd->value('categories_description');
+    $categories_keyword[$Qcd->valueInt('language_id')] = $Qcd->value('categories_keyword');
+    $categories_tags[$Qcd->valueInt('language_id')] = $Qcd->value('categories_tags');
+    $categories_meta_title[$Qcd->valueInt('language_id')] = $Qcd->value('categories_meta_title');
+    $categories_meta_keywords[$Qcd->valueInt('language_id')] = $Qcd->value('categories_meta_keywords');
+    $categories_meta_description[$Qcd->valueInt('language_id')] = $Qcd->value('categories_meta_description');
   }
 }
 
@@ -44,21 +51,66 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
   </style>
   <div class="with-padding-no-top">
     <form name="product" id="product" class="dataForm" action="<?php echo lc_href_link_admin(FILENAME_DEFAULT, $lC_Template->getModule() . '=' . (isset($lC_ObjectInfo) ? $lC_ObjectInfo->getInt('products_id') : '') . '&cID=' . $_GET['cID'] . '&action=save'); ?>" method="post" enctype="multipart/form-data">
-      <div class="standard-tabs same-height">
+      <div class="side-tabs">
         <ul class="tabs">
           <li class="active"><?php echo lc_link_object('#section_general_content', $lC_Language->get('section_general')); ?></li>
           <li id="tabHeaderSectionDataContent"><?php echo lc_link_object('#section_data_content', $lC_Language->get('section_data')); ?></li>
           <li><?php echo lc_link_object('#section_categories_content', $lC_Language->get('section_categories')); ?></li>
         </ul>
         <div class="clearfix tabs-content">
-          <div id="section_general_content" class="with-padding">
-            General
+          <div id="section_general_content">
+            <div class="columns">
+            
+            
+              <div class="twelve-columns large-margin-top">
+                <div class="with-padding">
+                  <div class="columns">
+                  
+                  </div>
+                </div>
+              </div>
+              <div class="twelve-columns large-margin-top">
+                <div class="field-drop-tabs button-height black-inputs">
+                  <div class="columns no-margin-bottom">
+                    <div class="one-column">
+                      <label class="label" for="resize_height"><b>Mode</b></label>
+                    </div>
+                    <div class="eleven-columns margin-bottom">
+                      <div>
+                        <input id="category_mode_1" type="radio" value="category" name="category_mode">
+                        <span>Product Category</span>
+                      </div>
+                      <div>
+                        <input id="category_mode_2" type="radio" value="page" name="category_mode">
+                        <span>Page Only</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="columns">
+                    <div class="one-column">
+                      <label class="label" for="resize_height"><b>Parent</b></label>
+                    </div>
+                    <div class="eleven-columns">
+                      <select class="select">
+                        <option>Top</option>
+                        <option>Women</option>
+                        <option>Men</option>
+                        <option>Kids</option>
+                        <option>Accessories</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              
+            </div>  
           </div>
           <div id="section_data_content" class="with-padding">
             Data
           </div>
           <div id="section_categories_content" class="with-padding">
-            Relatioships
+            Relationships
           </div>
         </div>
       </div>
