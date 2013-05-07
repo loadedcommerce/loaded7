dddc7d81bfb6c8b573b19d70c1b3d3b66b7bb2aa
gulsarrays <gulmohar@loadedcommerce.com>
2013-05-07 19:07:51 +0530

    D941 Admin Style - Selects (drop downs). Fixed. Updated few more pages

--- catalog/admin/includes/modules/product_attributes/manufacturers.php
+++ catalog/admin/includes/modules/product_attributes/manufacturers.php
@@ -31,7 +31,7 @@
       }
 
       if ( !empty($array) ) {
-        $string = lc_draw_pull_down_menu('attributes[' . self::getID() . ']', $array, $value, 'class="select" style = "width:47%"');
+        $string = lc_draw_pull_down_menu('attributes[' . self::getID() . ']', $array, $value, 'class="select" style = "width:25%"');
       }
 
       return $string;
