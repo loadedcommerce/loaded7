4a0e0db0ff62b072dd577ecc22809bda49affda0
datazen <scott.logsdon@loadedcommerce.com>
2013-05-28 18:18:10 -0400

    [IC] addition to last commit

--- catalog/admin/includes/applications/countries/pages/main.php
+++ catalog/admin/includes/applications/countries/pages/main.php
@@ -24,7 +24,7 @@
   .dataColCode { text-align: left; }
   .dataColTotal { text-align: right; }
   .dataColAction { text-align: right; }
-  .dataTables_info { bottom: 42px; color:#4c4c4c; }
+  .dataTables_info { position:absolute; bottom: 42px; color:#4c4c4c; }
   .selectContainer { position:absolute; bottom:29px; left:30px }
   </style>
   <div class="with-padding-no-top">
