7c024e6d6aee7e5bb9756e726d06db267055e1a0
geraldbullard <gerald.bullard@loadedcommerce.com>
2013-06-01 12:25:29 -0400

    more updates

--- catalog/admin/includes/applications/categories/js/categories.js.php
+++ catalog/admin/includes/applications/categories/js/categories.js.php
@@ -13,24 +13,7 @@
 */
 global $lC_Template;
 ?>
-<script>
-/* following two functions added for sorting against the <td> title="string" for icons or images only in column */
-jQuery.fn.dataTableExt.oSort['title-numeric-asc']  = function(a,b) {
-    var x = a.match(/title="*(-?[0-9]+)/)[1];
-    var y = b.match(/title="*(-?[0-9]+)/)[1];
-    x = parseFloat( x );
-    y = parseFloat( y );
-    return ((x < y) ? -1 : ((x > y) ?  1 : 0));
-};
- 
-jQuery.fn.dataTableExt.oSort['title-numeric-desc'] = function(a,b) {
-    var x = a.match(/title="*(-?[0-9]+)/)[1];
-    var y = b.match(/title="*(-?[0-9]+)/)[1];
-    x = parseFloat( x );
-    y = parseFloat( y );
-    return ((x < y) ?  1 : ((x > y) ? -1 : 0));
-};
-  
+<script>  
 function _refreshDataTable() {
   var paginationType = ($.template.mediaQuery.isSmallerThan('tablet-portrait')) ? 'two_button' : 'full_numbers';            
   var dataTableDataURL = '<?php echo lc_href_link_admin('rpc.php', $lC_Template->getModule() . '=' . $_GET[$lC_Template->getModule()] . '&action=getAll&media=MEDIA'); ?>';
