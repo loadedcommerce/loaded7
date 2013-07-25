1eb99efd681af71b3e5f51c0b3240ff15be5c0cb
datazen <scott.logsdon@loadedcommerce.com>
2013-07-25 12:10:25 -0400

    [IC] added coupon ot

--- catalog/admin/includes/modules/order_total/coupon.php
+++ catalog/admin/includes/modules/order_total/coupon.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: tax.php v1.0 2013-01-01 datazen $
+  $Id: coupon.php v1.0 2013-01-01 datazen $
 
   LoadedCommerce, Innovative eCommerce Solutions
   http://www.loadedcommerce.com
@@ -12,46 +12,46 @@
   @license    http://loadedcommerce.com/license.html
 */
 
-  class lC_OrderTotal_tax extends lC_Modules_order_total_Admin {
+  class lC_OrderTotal_coupon extends lC_Modules_order_total_Admin {
     var $_title,
-        $_code = 'tax',
-        $_author_name = 'LoadedCommerce',
+        $_code = 'coupon',
+        $_author_name = 'Loaded Commerce',
         $_author_www = 'http://www.loadedcommerce.com',
         $_status = false,
         $_sort_order;
 
-    public function lC_OrderTotal_tax() {
+    public function lC_OrderTotal_coupon() {
       global $lC_Language;
 
-      $this->_title = $lC_Language->get('order_total_tax_title');
-      $this->_description = $lC_Language->get('order_total_tax_description');
-      $this->_status = (defined('MODULE_ORDER_TOTAL_TAX_STATUS') && (MODULE_ORDER_TOTAL_TAX_STATUS == 'true') ? true : false);
-      $this->_sort_order = (defined('MODULE_ORDER_TOTAL_TAX_SORT_ORDER') ? MODULE_ORDER_TOTAL_TAX_SORT_ORDER : null);
+      $this->_title = $lC_Language->get('order_total_coupon_title');
+      $this->_description = $lC_Language->get('order_total_coupon_description');
+      $this->_status = (defined('MODULE_ORDER_TOTAL_COUPON_STATUS') && (MODULE_ORDER_TOTAL_COUPON_STATUS == 'true') ? true : false);
+      $this->_sort_order = (defined('MODULE_ORDER_TOTAL_COUPON_SORT_ORDER') ? MODULE_ORDER_TOTAL_COUPON_SORT_ORDER : null);
     }
 
     public function isInstalled() {
-      return (bool)defined('MODULE_ORDER_TOTAL_TAX_STATUS');
+      return (bool)defined('MODULE_ORDER_TOTAL_COUPON_STATUS');
     }
 
     public function install() {
       global $lC_Database;
 
-      $lC_Database->simpleQuery("insert ignore into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Display Tax', 'MODULE_ORDER_TOTAL_TAX_STATUS', 'true', 'Do you want to display the order tax value?', '6', '1', 'lc_cfg_set_boolean_value(array(\'true\', \'false\'))', now())");
-      $lC_Database->simpleQuery("insert ignore into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_ORDER_TOTAL_TAX_SORT_ORDER', '3', 'Sort order of display.', '6', '2', now())");
-      $lC_Database->simpleQuery("insert ignore into " . TABLE_TEMPLATES_BOXES . " (title, code, author_name, author_www, modules_group) values ('Tax', 'tax', 'LoadedComerce', 'http://www.loadedcommerce.com', 'order_total')");
+      $lC_Database->simpleQuery("insert ignore into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Display Coupon', 'MODULE_ORDER_TOTAL_COUPON_STATUS', '-1', 'Do you want to dusplay the coupon discount total on the checkout pages?', '6', '0', 'lc_cfg_use_get_boolean_value', 'lc_cfg_set_boolean_value(array(1, -1))', now())");
+      $lC_Database->simpleQuery("insert ignore into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_ORDER_TOTAL_COUPON_SORT_ORDER', '3', 'Sort order of the display.', '6', '0', now())");
+      $lC_Database->simpleQuery("insert ignore into " . TABLE_TEMPLATES_BOXES . " (title, code, author_name, author_www, modules_group) values ('Coupon', 'coupon', 'Loaded Comerce', 'http://www.loadedcommerce.com', 'order_total')");
     }
 
     public function remove() {
       global $lC_Database;
 
       $lC_Database->simpleQuery("delete from " . TABLE_CONFIGURATION . " where configuration_key in ('" . implode("', '", $this->getKeys()) . "')");
-      $lC_Database->simpleQuery("delete from " . TABLE_TEMPLATES_BOXES . " where code = 'tax'");
+      $lC_Database->simpleQuery("delete from " . TABLE_TEMPLATES_BOXES . " where code = 'coupon'");
     }
 
     public function getKeys() {
       if (!isset($this->_keys)) {
-        $this->_keys = array('MODULE_ORDER_TOTAL_TAX_STATUS',
-                             'MODULE_ORDER_TOTAL_TAX_SORT_ORDER');
+        $this->_keys = array('MODULE_ORDER_TOTAL_COUPON_STATUS',
+                             'MODULE_ORDER_TOTAL_COUPON_SORT_ORDER');
       }
 
       return $this->_keys;
