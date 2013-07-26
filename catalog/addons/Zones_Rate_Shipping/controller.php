16737590e73a904c8050d923507fab6126cf08dd
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-07-26 14:23:06 -0400

    Final updates before qa testing

--- catalog/addons/Zones_Rate_Shipping/controller.php
+++ catalog/addons/Zones_Rate_Shipping/controller.php
@@ -72,16 +72,13 @@ class Zones_Rate_Shipping extends lC_Addon {
    /**
     * The number of zones needed; also needs to match the value in the module
     */
-    if(defined('ADDONS_SHIPPING_' . strtoupper($this->_code) . '_NUMBER_OF_ZONES')){
-
+    if(defined('ADDONS_SHIPPING_' . strtoupper($this->_code) . '_NUMBER_OF_ZONES')) {
       $this->num_zones = ADDONS_SHIPPING_ZONES_RATE_SHIPPING_NUMBER_OF_ZONES;
-    }else{
-
+    } else {
       define('ADDONS_SHIPPING_' . strtoupper($this->_code) . '_NUMBER_OF_ZONES', '1');
       $this->num_zones = 1;
     }
-
-   }
+  }
  /**
   * Checks to see if the addon has been installed
   *
@@ -106,12 +103,8 @@ class Zones_Rate_Shipping extends lC_Addon {
     $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Module weight Unit', 'ADDONS_SHIPPING_" . strtoupper($this->_code) . "_WEIGHT_UNIT', '2', 'What unit of weight does this shipping module use?.', '6', '0', 'lC_Weight::getTitle', 'lc_cfg_set_weight_classes_pulldown_menu', now())");
     
     if (!defined("ADDONS_SHIPPING_" . strtoupper($this->_code) . "_NUMBER_OF_ZONES")) {
-      
       $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Number of World Zones', 'ADDONS_SHIPPING_" . strtoupper($this->_code) . "_NUMBER_OF_ZONES', '1', 'Number of World Zones to use.', '7', '0', now())");
-    }else{
-
-      $lC_Database->simpleQuery("update " . TABLE_CONFIGURATION . " SET configuration_value = '1' WHERE configuration_key = 'ADDONS_SHIPPING_ZONES_RATE_SHIPPING_NUMBER_OF_ZONES'");
-    }
+    } 
 
     for ($i = 1; $i <= $this->num_zones; $i++) {
       $default_countries = '';
@@ -141,7 +134,6 @@ class Zones_Rate_Shipping extends lC_Addon {
                              'ADDONS_SHIPPING_' . strtoupper($this->_code) . '_TAX_CLASS',
                              'ADDONS_SHIPPING_' . strtoupper($this->_code) . '_SORT_ORDER',
                              'ADDONS_SHIPPING_' . strtoupper($this->_code) . '_WEIGHT_UNIT');
-                             //'ADDONS_SHIPPING_' . strtoupper($this->_code) . '_NUMBER_OF_ZONES');
 
         for ($i=1; $i<=$this->num_zones; $i++) {
 
