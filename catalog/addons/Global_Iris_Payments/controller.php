f1485337f2d095757a3ca5e2a8122a3b074cdd68
datazen <scott.logsdon@loadedcommerce.com>
2013-08-01 09:43:55 -0400

    #1305 working on global iris

--- catalog/addons/Global_Iris_Payments/controller.php
+++ catalog/addons/Global_Iris_Payments/controller.php
@@ -61,7 +61,7 @@ class Global_Iris_Payments extends lC_Addon {
    /**
     * The mobile capability of the addon
     */ 
-    $this->_mobile_enabled = true;    
+    $this->_mobile_enabled = false;    
    /**
     * The addon enable/disable switch
     */    
