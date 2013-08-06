392133ad88c9ec329d9159ca9d1d3a7699c65734
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-08-06 14:00:20 -0400

    added blank .js.php

---
+++ catalog/admin/includes/applications/branding_manager/js/branding_manager.js.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id: branding_manager.js.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+?>
