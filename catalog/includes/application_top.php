d4161c9556ad3b471fbe4d5d6471792f7e57ed31
Gerald Bullard Jr <gerald.bullard@gmail.com>
2013-08-08 16:47:52 -0400

    Update application_top.php

--- catalog/includes/application_top.php
+++ catalog/includes/application_top.php
@@ -2,13 +2,10 @@
 /**
   $Id: application_top.php v1.0 2013-01-01 datazen $
 
-
   LoadedCommerce, Innovative eCommerce Solutions
   http://www.loadedcommerce.com
 
-
   Copyright (c) 2013 Loaded Commerce, LLC
-
 
   @author     LoadedCommerce Team
   @copyright  (c) 2013 LoadedCommerce Team
