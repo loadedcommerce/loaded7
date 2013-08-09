47b8b35d051a186f142d0b34922af8490525e56c
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-08-09 16:47:17 -0400

    more updates

--- catalog/admin/includes/applications/orders/pages/edit.php
+++ catalog/admin/includes/applications/orders/pages/edit.php
@@ -46,12 +46,33 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
         </ul>
         <div class="clearfix tabs-content" id="orders_sections">
           <div id="section_orders_summary">
+            <h3 class="show-below-768 margin-left margin-top no-margin-bottom">Summary</h3>
             <div class="columns with-padding">
               <div class="new-row-mobile six-columns twelve-columns-mobile">
                 <fieldset>
                   <legend class="small-margin-bottom">
                     <span class="icon-user icon-anthracite"><strong class="small-margin-left"><?php echo $lC_Language->get('orders_summary_customer'); ?></strong></span>
                   </legend>
+                  <table width="100%" cellpadding="0" cellspacing="0">
+                    <tbody>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Name</td>
+                        <td class="align-left pad-5 width-66 bold">Sal Iozzia</td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Company Name</td>
+                        <td class="align-left pad-5 width-66 bold"><small class="tag orange-bg">B2B</small></td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">City, State</td>
+                        <td class="align-left pad-5 width-66 bold">Atlanta, GA USA</td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Customer Group</td>
+                        <td class="align-left pad-5 width-66 bold">Retail</td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </fieldset>
               </div>
               <div class="new-row-mobile six-columns twelve-columns-mobile">
@@ -59,13 +80,47 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
                   <legend class="small-margin-bottom">
                     <span class="icon-credit-card icon-anthracite"><strong class="small-margin-left"><?php echo $lC_Language->get('orders_summary_payment'); ?></strong></span>
                   </legend>
+                  <table width="100%" cellpadding="0" cellspacing="0">
+                    <tbody>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Method</td>
+                        <td class="align-left pad-5 width-66 bold">Credit Card via Loaded Payments</td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Details</td>
+                        <td class="align-left pad-5 width-66 bold">Sal Iozzia - Visa - 4111</td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Payment State</td>
+                        <td class="align-left pad-5 width-66 bold">PAID</td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </fieldset>
               </div>
+            </div>
+            <div class="columns with-padding large-pull-top">
               <div class="new-row-mobile six-columns twelve-columns-mobile">
                 <fieldset>
                   <legend class="small-margin-bottom">
                     <span class="icon-info-round icon-anthracite"><strong class="small-margin-left"><?php echo $lC_Language->get('orders_summary_info'); ?></strong></span>
                   </legend>
+                  <table width="100%" cellpadding="0" cellspacing="0">
+                    <tbody>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Addresses</td>
+                        <td class="align-left pad-5 width-66 bold">Mixed</td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Date Ordered</td>
+                        <td class="align-left pad-5 width-66 bold">2013/08/04</td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Purchase Order</td>
+                        <td class="align-left pad-5 width-66 bold"><small class="tag orange-bg">B2B</small></td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </fieldset>
               </div>
               <div class="new-row-mobile six-columns twelve-columns-mobile">
@@ -73,6 +128,22 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
                   <legend class="small-margin-bottom">
                     <span class="icon-dropbox icon-anthracite"><strong class="small-margin-left"><?php echo $lC_Language->get('orders_summary_shipping'); ?></strong></span>
                   </legend>
+                  <table width="100%" cellpadding="0" cellspacing="0">
+                    <tbody>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Method</td>
+                        <td class="align-left pad-5 width-66 bold">FedEx</td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Type</td>
+                        <td class="align-left pad-5 width-66 bold">Next Day Air</td>
+                      </tr>
+                      <tr>
+                        <td class="align-right pad-5 width-33 bold grey">Shipping State</td>
+                        <td class="align-left pad-5 width-66 bold">Backordered</td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </fieldset>
               </div>
             </div>
@@ -105,30 +176,10 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
                     </thead>
                     <tbody class="mid-margin-bottom">
                     <?php
-                      $Qordersproducts = array(array('id' => '24',
-                                                     'model' => 'Floral001RL', 
-                                                     'name' => 'Floral Dress - Red - Large', 
-                                                     'stock' => 'In Stock', 
-                                                     'qty' => '2', 
-                                                     'total' => '250.00'),
-                                               array('id' => '25',
-                                                     'model' => 'Watch001', 
-                                                     'name' => 'Mens Watch', 
-                                                     'stock' => 'Shipped', 
-                                                     'qty' => '1', 
-                                                     'total' => '100.00'),
-                                               array('id' => '26',
-                                                     'model' => 'GC124GIFT', 
-                                                     'name' => 'Gift Certificate $125', 
-                                                     'stock' => '-', 
-                                                     'qty' => '1', 
-                                                     'total' => '125.00'),
-                                               array('id' => '27',
-                                                     'model' => 'Floral001GXL', 
-                                                     'name' => 'Floral Dress - Green - Large', 
-                                                     'stock' => 'Out of Stock', 
-                                                     'qty' => '1', 
-                                                     'total' => '125.00')); 
+                      $Qordersproducts = array(array('id' => '24', 'model' => 'Floral001RL', 'name' => 'Floral Dress - Red - Large', 'stock' => 'In Stock', 'qty' => '2', 'total' => '250.00'),
+                                               array('id' => '25', 'model' => 'Watch001', 'name' => 'Mens Watch', 'stock' => 'Shipped', 'qty' => '1', 'total' => '100.00'),
+                                               array('id' => '26', 'model' => 'GC124GIFT', 'name' => 'Gift Certificate $125', 'stock' => '-', 'qty' => '1', 'total' => '125.00'),
+                                               array('id' => '27', 'model' => 'Floral001GXL', 'name' => 'Floral Dress - Green - Large', 'stock' => 'Out of Stock', 'qty' => '1', 'total' => '125.00')); 
                       foreach ($Qordersproducts as $products) {
                       ?>
                       <tr id="orderProductsRow_<?php echo $products['id']; ?>" class="bbottom-grey">
@@ -146,8 +197,8 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
                         <td class="hide-below-480"></td>
                         <td class="hide-below-480"></td>
                         <td class="hide-below-480"></td>
-                        <td class="no-wrap pad-right-10 pad-top-10 bold" align="right">Product Sub Total</td>
-                        <td class="pad-top-10 bold" align="right">$600.00</td>
+                        <td class="no-wrap pad-right-10 pad-top-10 bolder" align="right">Product Sub Total</td>
+                        <td class="pad-top-10 bolder" align="right">$600.00</td>
                         <td class="show-below-480"></td>
                       </tr>
                     </tbody>
@@ -158,7 +209,7 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
             <div class="columns">
               <div class="twelve-columns large-pull-top">
                 <div class="columns with-padding mid-margin-top">
-                  <div class="six-columns twelve-columns-mobile">
+                  <div class="six-columns twelve-columns-mobile hide-below-768">
                     <fieldset>
                       <legend class="margin-bottom">
                         <span class="icon-chat icon-size2 icon-anthracite mid-margin-left"><strong class="small-margin-left"><?php echo $lC_Language->get('orders_summary_recent_messages'); ?></strong></span>
@@ -212,6 +263,28 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
                           <legend class="small-margin-bottom">
                             <span class="icon-credit-card icon-size2 icon-anthracite"><strong class="small-margin-left"><?php echo $lC_Language->get('orders_summary_order_totals'); ?></strong></span>
                           </legend>
+                          <div class="large-margin-left">
+                            <table width="100%" cellpadding="0" cellspacing="0">
+                              <tbody>
+                                <tr>
+                                  <td class="align-right pad-5 width-75">Coupon 10% Off</td>
+                                  <td class="align-right pad-5 width-25">-$62.50</td>
+                                </tr>
+                                <tr>
+                                  <td class="align-right pad-5 width-75">Shipping Fee - FedEx</td>
+                                  <td class="align-right pad-5 width-25">$25.00</td>
+                                </tr>
+                                <tr>
+                                  <td class="align-right pad-5 width-75">Tax</td>
+                                  <td class="align-right pad-5 width-25">$10.00</td>
+                                </tr>
+                                <tr>
+                                  <td class="align-right pad-5 width-75 bold btop-grey">Grand Total</td>
+                                  <td class="align-right pad-5 width-25 bold btop-grey">$572.50</td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
                         </fieldset>
                       </div>
                       <div class="twelve-columns">
@@ -219,10 +292,73 @@ if ( is_numeric($_GET[$lC_Template->getModule()]) ) {
                           <legend class="small-margin-bottom">
                             <span class="icon-credit-card icon-size2 icon-anthracite"><strong class="small-margin-left"><?php echo $lC_Language->get('orders_summary_last_payment'); ?></strong></span>
                           </legend>
+                          <div class="large-margin-left">
+                            <table width="100%" cellpadding="0" cellspacing="0">
+                              <tbody>
+                                <tr>
+                                  <td class="align-right pad-5 width-75">2013/08/07 Credit Card 2345</td>
+                                  <td class="align-right pad-5 width-25">$572.50</td>
+                                </tr>
+                                <tr>
+                                  <td class="align-right pad-5 width-75 bold btop-grey">Balance</td>
+                                  <td class="align-right pad-5 width-25 bold btop-grey">$0.00</td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
                         </fieldset>
                       </div>
                     </div>
                   </div>
+                  <!-- duplicate messages for mobile AFTER totals :: START -->
+                  <div class="six-columns twelve-columns-mobile show-below-768">
+                    <fieldset>
+                      <legend class="margin-bottom">
+                        <span class="icon-chat icon-size2 icon-anthracite mid-margin-left"><strong class="small-margin-left"><?php echo $lC_Language->get('orders_summary_recent_messages'); ?></strong></span>
+                      </legend>
+                      <div class="with-small-padding silver-bg">
+                        <div class="small-margin-top">
+                          <span class="float-right green-bg with-min-padding">Comment</span>
+                          <span class="icon-user icon-size2 icon-anthracite small-margin-left">
+                            <span>
+                              Sal Iozzia <small class="anthracite small-margin-left">2013/08/07</small>
+                            </span>
+                          </span>
+                        </div>
+                        <p class="edit-order-summary-block with-small-padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula est ut interdum fringilla. Curabitur lobortis libero ut sagittis fermentum. Donec eu tortor ut elit condimentum iaculis. Nulla semper diam ut mi volutpat pharetra.</p>
+                      </div>
+                      <div class="with-small-padding">
+                        <div class="small-margin-top">
+                          <span class="float-right orange-bg with-min-padding">Message</span>
+                          <span>
+                            <img src="images/prod-mini.png" alt="Comment Image" />
+                            <span class="small-margin-left">
+                              John Nickelback <small class="anthracite small-margin-left">2013/08/05</small>
+                            </span>
+                          </span>
+                        </div>
+                        <p class="edit-order-summary-block with-small-padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula est ut interdum fringilla. Curabitur lobortis libero ut sagittis fermentum. Donec eu tortor ut elit condimentum iaculis. Nulla semper diam ut mi volutpat pharetra.</p>
+                      </div>
+                      <div class="with-small-padding grey-bg">
+                        <div class="small-margin-top">
+                          <span class="float-right anthracite-bg with-min-padding">Note</span>
+                          <span>
+                            <img src="images/prod-mini.png" alt="Comment Image" />
+                            <span class="small-margin-left">
+                              Jane Quarterback <small class="anthracite small-margin-left">sent 2013/08/02</small>
+                            </span>
+                          </span>
+                        </div>
+                        <p class="edit-order-summary-block with-small-padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula est ut interdum fringilla. Curabitur lobortis libero ut sagittis fermentum. Donec eu tortor ut elit condimentum iaculis. Nulla semper diam ut mi volutpat pharetra.</p>
+                      </div>
+                      <div class="with-small-padding">
+                        <div class="small-margin-top">
+                          <i class="icon-mail"></i> <span class="small-margin-left">2 Additional Messages</span>
+                        </div>
+                      </div>
+                    </fieldset>
+                  </div>
+                  <!-- duplicate messages for mobile AFTER totals :: END -->
                 </div>
               </div>
             </div>
