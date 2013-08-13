cd808e0337c5fafeab9b0cc6ab811cde59c0f78e
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-08-13 12:17:46 -0400

    starting customer tab layout

--- catalog/admin/includes/applications/orders/pages/tabs/customer.php
+++ catalog/admin/includes/applications/orders/pages/tabs/customer.php
@@ -14,4 +14,31 @@
 ?>
           <div id="section_orders_customer">
             <h3 class="show-below-768 margin-left margin-top no-margin-bottom">Customer</h3>
+            <div class="columns with-padding">
+              <div class="new-row-mobile twelve-columns twelve-columns-mobile">
+                <fieldset class="fieldset">
+                  <legend class="legend">Customer info</legend>
+                  <div class="columns with-small-padding">
+                    <div class="new-row-mobile three-columns twelve-columns-mobile">
+                    Left
+                    </div>
+                    <div class="new-row-mobile six-columns twelve-columns-mobile">
+                    Right
+                    </div>
+                    <div class="new-row-mobile three-columns twelve-columns-mobile">
+                      <span class="button-group">
+                        <a href="#" class="button compact">View</a>
+                        <a href="#" class="button compact">Edit</a>
+                      </span>
+                    </div>
+                  </div>
+                </fieldset>
+                <fieldset class="fieldset">
+                  <legend class="legend">Alternate Customer info</legend>
+                </fieldset>
+                <fieldset class="fieldset">
+                  <legend class="legend">Order History</legend>
+                </fieldset>
+              </div>
+            </div>
           </div>
