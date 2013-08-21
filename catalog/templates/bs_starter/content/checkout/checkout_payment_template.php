26ccf40e74c19746a19bf74b73f34d0a9437e16b
datazen <scott.logsdon@loadedcommerce.com>
2013-08-21 13:18:20 -0400

    [IC] bootstrap work

--- catalog/templates/bs_starter/content/checkout/checkout_payment_template.php
+++ catalog/templates/bs_starter/content/checkout/checkout_payment_template.php
@@ -1,31 +1,19 @@
 <?php
 /**  
-*  $Id: checkout_payment_template.php v1.0 2013-01-01 datazen $
-*
-*  LoadedCommerce, Innovative eCommerce Solutions
-*  http://www.loadedcommerce.com
-*
-*  Copyright (c) 2013 Loaded Commerce, LLC
-*
-*  @author     Loaded Commerce Team
-*  @copyright  (c) 2013 Loaded Commerce Team
-*  @license    http://loadedcommerce.com/license.html
-*/
-$secureUrl = ($lC_Payment->hasIframeURL()) ? substr($lC_Payment->getIframeURL(), 0, strpos($lC_Payment->getIframeURL(), '?')) : (($lC_Payment->hasRelayURL()) ?  $lC_Payment->getRelayURL() : NULL);
+  @package    catalog::templates::content
+  @author     Loaded Commerce, LLC
+  @copyright  Copyright 2003-2013 Loaded Commerce Development Team
+  @copyright  Portions Copyright 2003 osCommerce
+  @copyright  Template built on DevKit http://www.bootstraptor.com under GPL license 
+  @license    https://github.com/loadedcommerce/loaded7/blob/master/LICENSE.txt
+  @version    $Id: checkout_payment_template.php v1.0 2013-08-08 datazen $
+*/ 
+$lC_Template->addJavascriptFilename('ext/jquery/jquery.activity-indicator-1.0.0.min.js');
 ?>
 <!--content/checkout/checkout_payment_template.php start-->
 <style>
 /*content area width, it is needed*/
-.colMid { width:75%; }
-#checkoutConfirmationDetails { background-color: #F9F8F6; border: 1px solid #EBE2D9; padding-top: 4px; width: 100%; }
-#paymentTemplateContainer .security-info-title { cursor:pointer; float:right; }
-#paymentTemplateContainer .security-info-text-container { margin:0 -5px 10px 0; background-color:#edfbec; }
-#paymentTemplateContainer .security-info-url { border:1px solid #ccc; padding:5px; font-weight:bold; }
-#paymentTemplateContainer .security-info-text { border:1px solid #ccc; padding:7px 5px; }
-#paymentTemplateContainer .arrow-container { margin-top:15px; }
-#paymentTemplateContainer .arrow-down { border-left: 10px solid transparent; border-right: 10px solid transparent; border-top: 10px solid gray; float: right; height: 0; width: 0; margin:13px 0 0 6px; }
-#paymentTemplateContainer .arrow-up { border-bottom: 10px solid gray; border-left: 10px solid transparent; border-right: 10px solid transparent; float: right; height: 0; width: 0; margin:13px 0 0 6px; } 
-#loadingContainer { position:absolute; right:250px; }
+#loadingContainer { position:absolute; left:50%; top:10%; }
 #iloader { margin:100px 0 0 0px; }
 <?php 
 // common
@@ -41,12 +29,7 @@ if ($lC_ShoppingCart->getBillingMethod('id') == 'paypal_adv') {
            $lC_ShoppingCart->getBillingMethod('id') == 'usaepay_cc' ) {
   $fHeight = '400px';
   $fScroll = 'auto';
-  echo "#checkout_shipping_col1 { width:28% !important; }";
-  echo "#checkout_shipping_col2 { width:71% !important; }";  
-} else {
-  echo "#checkout_shipping_col1 { width:28% !important; }";
-  echo "#checkout_shipping_col2 { width:71% !important; }";
-}
+} 
 ?>
 
 /* Mobile (landscape) ----------- */
@@ -101,63 +84,65 @@ only screen and (min-device-pixel-ratio : 1.5) {
 /* Styles */
 }
 </style>
-<div id="paymentTemplateContainer" class="full_page">
-  <h5><?php echo $lC_Language->get('text_checkout'); ?></h5>
-  <div class="checkout_steps">
-    <ol id="checkoutSteps">
-      <li class="first-checkout-li"> <a href="<?php echo lc_href_link(FILENAME_CHECKOUT, 'shipping', 'SSL'); ?>">
-        <div class="step-title">
-          <h2><?php echo $lC_Language->get('box_ordering_steps_delivery'); ?></h2>
+
+<!--content/checkout/checkout_payment_template.php start-->
+<div class="row">
+  <div class="col-sm-12 col-lg-12 large-margin-bottom">  
+    <h1 class="no-margin-top"><?php echo $lC_Language->get('text_checkout'); ?></h1>
+    <div id="content-checkout-shipping-container">
+      <div class="panel panel-default no-margin-bottom">
+        <div class="panel-heading cursor-pointer" onclick="window.location.href='<?php echo lc_href_link(FILENAME_CHECKOUT, 'shipping', 'SSL'); ?>'">
+          <h3 class="no-margin-top no-margin-bottom"><?php echo $lC_Language->get('box_ordering_steps_delivery'); ?></h3>
         </div>
-        </a> 
-      </li>
-      <li> <a href="<?php echo lc_href_link(FILENAME_CHECKOUT, 'payment', 'SSL'); ?>">
-        <div class="step-title">
-          <h2><?php echo $lC_Language->get('box_ordering_steps_payment'); ?></h2>
+      </div>
+      <div class="clearfix panel panel-default no-margin-bottom">
+        <div class="panel-heading cursor-pointer" onclick="window.location.href='<?php echo lc_href_link(FILENAME_CHECKOUT, 'payment&skip=no', 'SSL'); ?>'">
+          <h3 class="no-margin-top no-margin-bottom"><?php echo $lC_Language->get('box_ordering_steps_payment'); ?></h3>
         </div>
-        </a> 
-      </li>
-      <li class="section allow active">
-        <div class="step-title">
-          <h2><?php echo $lC_Language->get('box_ordering_steps_secure_checkout'); ?></h2>
-        </div>
-        <div id="checkout-step-login">
-          <p><?php echo $lC_Language->get('secure_payment_description_text'); ?></p>
-          <script>
-            /* we use javascript here due to js scrubbing at payment endpoints - we don't want to show if javascript is disabled or scrubbed */
-            var output = '<div class="security-info-container">'+
-                         '<div class="security-info-title" onclick="toggleSecurityInfo();"><?php echo lc_image('images/greenlock.png', null, null, null, 'style="vertical-align:middle; margin:10px 5px;"') . $lC_Language->get('secure_payment_security_info_title'); ?><span class="arrow-container"><span id="arrow" class="arrow-down"></span></span></div>'+
-                         '<div style="clear:both;"></div>'+
-                         '<div class="security-info-text-container" style="display:none;">'+
-                         '  <div class="security-info-url"><?php echo lc_image('images/greenlock.png', null, null, null, 'style="vertical-align:middle; margin-right:5px;"') . $secureUrl; ?></div>'+
-                         '  <div class="security-info-text"><?php echo $lC_Language->get('secure_payment_security_info_text'); ?></div>'+
-                         '</div></div><div style="clear:both;"></div>';
-            
-            document.write(output);            
-          </script>                   
-          <div class="col2-set">
-            <div id="checkout_shipping_col1" style="width:28%; float:left;">
-              <div id="ot-container">
-                <div class="ot-block" id="order-number">
-                  <label><?php echo $lC_Language->get('checkout_order_number'); ?></label>
-                  <span><?php echo $_SESSION['cartID']; ?></span> 
-                </div>
+      </div>
+      <div class="clearfix panel panel-default no-margin-bottom">
+        <div class="panel-heading">
+          <h3 class="no-margin-top no-margin-bottom"><?php echo $lC_Language->get('box_ordering_steps_secure_checkout'); ?></h3>
+        </div>  
+        <div class="panel-body no-padding-bottom">
+          <div class="row">
+            <div class="col-sm-12 col-lg-12">
+              <div class="alert alert-warning no-margin-bottom">
+                <?php echo $lC_Language->get('secure_payment_description_text'); ?>
               </div>
-              <div id="ot-container" class="margin-top">
-                <div class="ot-block" id="order-number">
-                  <label>Amount Due</label>
-                  <span><?php echo $lC_Currencies->format($lC_ShoppingCart->getTotal()); ?></span>
-                </div>
-              </div>
-              <div id="bill-to-address-block">
-                <h3><?php echo $lC_Language->get('bill_to_address'); ?></h3>
-                <span style="float:right;"><a href="<?php echo lc_href_link(FILENAME_CHECKOUT, 'payment_address', 'SSL'); ?>" class="sc-button small grey colorWhite noDecoration"><?php echo $lC_Language->get('button_edit'); ?></a></span> <span id="bill-to-span"><?php echo lC_Address::format($lC_ShoppingCart->getBillingAddress(), '<br />'); ?></span>
-              </div>
-              <?php
-                //echo '[' . $_SESSION['mediaType'] . '][' . $_SESSION['mediaSize'] . ']<br>';
-              ?>
+              <script>
+                /* we use javascript here due to js scrubbing at payment endpoints - we don't want to show if javascript is disabled or scrubbed */
+                var output = '<div class="security-info-container">'+
+                             '  <div class="security-info-title text-right cursor-pointer" onclick="$(\'#security-info-alert\').toggle(\'slideDown\');"><?php echo lc_image('images/greenlock.png', null, null, null, 'style="vertical-align:middle; margin:10px 5px;"') . $lC_Language->get('secure_payment_security_info_title'); ?><span class="arrow-container"><span id="arrow" class="arrow-down"></span></span></div>'+
+                             '  <div id="security-info-alert" class="alert alert-success" style="display:none;">'+
+                             '    <div class="security-info-url"><?php echo lc_image('images/greenlock.png', null, null, null, 'style="vertical-align:middle; margin-right:5px;"') . ($lC_Payment->hasIframeURL()) ? substr($lC_Payment->getIframeURL(), 0, strpos($lC_Payment->getIframeURL(), '?')) : (($lC_Payment->hasRelayURL()) ?  $lC_Payment->getRelayURL() : NULL); ?></div>'+
+                             '    <div class="security-info-text normal large-margin-left small-padding-left"><?php echo $lC_Language->get('secure_payment_security_info_text'); ?></div>'+
+                             '  </div>' +
+                             '</div>';
+                
+                document.write(output);            
+              </script>   
             </div>
-            <div id="checkout_shipping_col2" style="width:71%; float:right; margin-right:-4px;">
+            <div class="col-sm-4 col-lg-4">
+              <div class="well">
+                <span class="strong"><?php echo $lC_Language->get('checkout_order_number'); ?></span>
+                <span class="pull-right strong"><?php echo $_SESSION['cartID']; ?></span> 
+              </div>
+              <div class="well">
+                <span class="strong"><?php echo $lC_Language->get('text_amount_due'); ?></span>
+                <span class="pull-right strong"><?php echo $lC_Currencies->format($lC_ShoppingCart->getTotal()); ?></span>
+              </div>
+              <div class="well relative no-padding-bottom">
+                <h4 class="no-margin-top"><?php echo $lC_Language->get('bill_to_address'); ?></h4>
+                <address>
+                  <?php echo lC_Address::format($lC_ShoppingCart->getBillingAddress(), '<br />'); ?>                
+                </address>
+                <div class="btn-group clearfix absolute-top-right">
+                  <a href="<?php echo lc_href_link(FILENAME_CHECKOUT, 'payment_address', 'SSL'); ?>"><button type="button" class="btn btn-default btn-xs"><?php echo $lC_Language->get('button_edit'); ?></button></a>
+                </div>                  
+              </div>
+            </div>
+            <div class="col-sm-8 col-lg-8 no-padding-left large-margin-bottom">
               <div id="checkoutConfirmationDetails"> 
                 <div id="loadingContainer"><p id="iloader"></p></div>
                 <?php  
@@ -171,14 +156,13 @@ only screen and (min-device-pixel-ratio : 1.5) {
                 }
               ?>
               </div>
-            </div>     
-          </div>
-        </div> 
-      </li>
-    </ol>
+            </div>
+          </div>        
+        </div>
+      </div>
+    </div> 
   </div>
-</div>
-<div style="clear:both;"></div>
+</div> 
 <script>
 function hideLoader() {
   var loadDiv = document.getElementById("loadingContainer"); 
