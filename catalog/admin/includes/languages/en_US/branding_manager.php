e960b3f5b93e59513a761372d4628e268cbdab01
wa4u <kiran@thereactorworks.com>
2013-08-22 16:59:36 +0530

    [D853] Banding Manager Language Updates

--- catalog/admin/includes/languages/en_US/branding_manager.php
+++ catalog/admin/includes/languages/en_US/branding_manager.php
@@ -10,11 +10,9 @@
 #  @license    http://loadedcommerce.com/license.html
 
 heading_title = Branding Manager 
-
-field_name = Group Name:
-field_set_as_default = Set as Default?
 field_header = Header
 field_branding_manager_logo = Catalog Logo :
+info_bubble_branding_manager_logo = Catalog Logo image <br>Allowed Extensions: jpg, jpeg, png, gif
 field_site_name = Site Name :
 field_site_slogan = Site Slogan :
 field_live_chat_code = Live Chat Code :
@@ -28,11 +26,10 @@ field_seo = SEO
 field_site_meta_description = Site Meta Description :
 field_site_meta_keywords = Site Meta Keywords :
 field_open_graph_site_thumbnail = Open Graph Site Thumbnail :
-field_open_graph_image = Open Graph image must be larger than 200 x 200
+info_bubble_open_graph_site_thumbnail = Open Graph image must be larger than 200 x 200<br>Allowed Extensions: jpg, jpeg, png, gif
 field_home_page_meta_title = Home Page Meta Title :
-field_site_meta_title_slug = Site Meta Title Slug :
-field_prefix = Prefix
-field_suffix = Suffix
+field_site_meta_title_prefix = Site Meta Title Prefix :
+field_site_meta_title_suffix = Site Meta Title Suffix :
 field_site_meta_title_delimeter = Site Meta Title Delimeter :
 field_footer_text = Footer Text
 field_site_footer_text = Site Footer Text :
