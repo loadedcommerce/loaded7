3ab286a16db3c120697633ae9d89e1dc75e38919
datazen <scott.logsdon@loadedcommerce.com>
2013-09-08 11:36:00 -0400

    [IC] code cleanup

--- catalog/templates/bs_starter/content/checkout/checkout_payment_address.php
+++ catalog/templates/bs_starter/content/checkout/checkout_payment_address.php
@@ -85,7 +85,7 @@
                           }
                           ?>
                           <td class=""><span class="strong"><?php echo $Qaddresses->valueProtected('firstname') . ' ' . $Qaddresses->valueProtected('lastname'); ?></span><br /><small><?php echo str_replace($Qaddresses->valueProtected('firstname') . ' ' . $Qaddresses->valueProtected('lastname') . ', ', '', lC_Address::format($Qaddresses->toArray(), ', ')); ?></small></td>
-                          <td class="text-right"><?php echo lc_draw_radio_field('address', $Qaddresses->valueInt('address_book_id'), $lC_ShoppingCart->getShippingAddress('id'),null,''); ?></td>
+                          <td class="text-right"><?php echo lc_draw_radio_field('address', $Qaddresses->valueInt('address_book_id'), $lC_ShoppingCart->getBillingAddress('id'), null, 'id="address_' . $lC_ShoppingCart->getShippingAddress('id') . '"'); ?></td>
                           </tr>
                           </table>
                           <?php
