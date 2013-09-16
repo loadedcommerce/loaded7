6186832888a00aa788ccefc8c5ce05fd91196a6e
wa4u <kiran@thereactorworks.com>
2013-09-17 01:15:58 +0530

    [D853] Language updates

--- catalog/admin/includes/languages/en_US/branding_manager.php
+++ catalog/admin/includes/languages/en_US/branding_manager.php
@@ -13,66 +13,66 @@ heading_title = Branding Manager
 text_drag_drop_to_replace = Drag & Drop or&nbsp;
 
 tab_header = Header
-field_branding_manager_logo = Catalog Logo :
-field_site_name = Site Name :
-field_site_slogan = Site Slogan :
-field_live_chat_code = Live Chat Code :
+field_branding_manager_logo = Catalog Logo:
+field_site_name = Site Name:
+field_site_slogan = Site Slogan:
+field_live_chat_code = Live Chat Code:
 
 info_bubble_branding_manager_logo = Catalog Logo image <br>Allowed Extensions: jpg, jpeg, png, gif
-info_bubble_site_name = Your Store Name.
-info_bubble_site_slogan = Your store site slogan.
-info_bubble_live_chat_code = Javascript or html code to show Live Chat option.
+info_bubble_site_name = This is the name of your site. This entry will show the same for all languages, it is not a multi-language setting.
+info_bubble_site_slogan = This is the "Catch Phrase" or "Mantra" for your site. It will show on the customer facing pages below or near the site logo, according to your template design. This is a multi-language setting.
+info_bubble_live_chat_code =  This is the setting to store your third-party live chat html/javascript code that can display your Live Chat help button or link. This is not a multi-language setting.
 
 tab_company_info = Company Info 
-field_address = Address :
-field_support_phone = Support Phone :
-field_support_email = Support Email :
-field_sales_phone = Sales Phone :
-field_sales_email = Sales Email :
+field_address = Address:
+field_support_phone = Support Phone:
+field_support_email = Support Email:
+field_sales_phone = Sales Phone:
+field_sales_email = Sales Email:
 
-info_bubble_address = Company postal Address.
-info_bubble_support_phone = Customer Support Phone Number.
-info_bubble_support_email = Customer Support Email address.
-info_bubble_sales_phone = Sales Phone Number.
-info_bubble_sales_email = Sales Email address.
+info_bubble_address = This is the main address for your site. It will be shown anywhere your site address is needed, like invoices, packing slips, emails etc. This is not a multi-language setting.
+info_bubble_support_phone = This is your site support phone number. This is not a multi-language setting.
+info_bubble_support_email = This is your site support email address. This is not a multi-language setting.
+info_bubble_sales_phone = This is your site sales phone number. This is not a multi-language setting.
+info_bubble_sales_email = This is your site sales email address. This is not a multi-language setting.
 
 
 tab_seo = SEO
-field_site_meta_description = Site Meta Description :
-field_site_meta_keywords = Site Meta Keywords :
-field_open_graph_site_thumbnail = Open Graph Site Thumbnail :
-field_home_page_meta_title = Home Page Meta Title :
-field_site_meta_title_prefix = Site Meta Title Prefix :
-field_site_meta_title_suffix = Site Meta Title Suffix :
-field_site_meta_title_delimeter = Site Meta Title Delimeter :
+field_site_meta_description = Site Meta Description:
+field_site_meta_keywords = Site Meta Keywords:
+field_open_graph_site_thumbnail = Open Graph Site Thumbnail:
+field_home_page_meta_title = Home Page Meta Title:
+field_site_meta_title_prefix = Site Meta Title Prefix:
+field_site_meta_title_suffix = Site Meta Title Suffix:
+field_site_meta_title_delimeter = Site Meta Title Delimeter:
 
 
-info_bubble_site_meta_description = Default Site Meta Description.
-info_bubble_site_meta_keywords = Default Site Meta Keywords used for store home page. 
+info_bubble_site_meta_description = This is your Site Meta Description. This is a multi-language setting.
+info_bubble_site_meta_keywords = These are your Site Meta Keywords. This is a multi-language setting. 
 infobubble_filed_open_graph_site_thumbnail = Open Graph image must be larger than 200 x 200<br>Allowed Extensions: jpg, jpeg, png, gif
-info_bubble_home_page_meta_title = Home Page Meta Title 
-info_bubble_site_meta_title_prefix = Site Meta Title Prefix infobubble
-info_bubble_site_meta_title_suffix = Site Meta Title Suffix infobubble
-info_bubble_site_meta_title_delimeter = Site Meta Title Delimeter infobubble
+info_bubble_home_page_meta_title = This is your Home Page Meta Title. This is a multi-language setting. 
+info_bubble_site_meta_title_prefix = This is your Site Meta Title Prefix. This is a multi-language setting.
+info_bubble_site_meta_title_suffix = This is your Site Meta Title Suffix. This is a multi-language setting.
+info_bubble_site_meta_title_delimeter = This is your Site Meta Title Delimeter. This is not a multi-language setting.
 
 
 tab_footer_text = Footer Text
-field_site_footer_text = Site Footer Text :
+field_site_footer_text = Site Footer Text:
 
-info_bubble_site_footer_text = Site Footer Text infobubble
+info_bubble_site_footer_text = This is the text/code for your site footer area. It can include text, html, javascript or any non-dynaimcally generated code. This is a multi-language setting.
 
 
 tab_social_links = Social Links
-field_facebook_page = facebook Page :
-field_twitter = Twitter :
-field_pinterest = Pinterest :
-field_google_plus = Google + :
-field_youtube = Youtube :
-field_linkedin = Linked In :
+field_facebook_page = Facebook Page:
+field_twitter = Twitter:
+field_pinterest = Pinterest:
+field_google_plus = Google+:
+field_youtube = YouTube:
+field_linkedin = LinkedIn:
 
-info_bubble_facebook_page = Your Facebook timeline url
-info_bubble_twitter = Your Twitter url
-info_bubble_pinterest = Your Pinterest url
-info_bubble_google_plus = Url to Google + page
-info_bubble_youtube = Your Youtube page url
-info_bubble_linkedin = Url to LinkedIn Profile
+info_bubble_facebook_page = This is your Facebook url. This is not a multi-language setting.
+info_bubble_twitter = This is your Twitter url. This is not a multi-language setting.
+info_bubble_pinterest = This is your Pinterest url. This is not a multi-language setting.
+info_bubble_google_plus = This is your Google+ url. This is not a multi-language setting.
+info_bubble_youtube = This is your YouTube url. This is not a multi-language setting.
+info_bubble_linkedin = This is your LinkedIn url. This is not a multi-language setting.
