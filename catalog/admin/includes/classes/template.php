0924897f4c6ca93411035e964838d92f7e3113f0
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-09-17 12:01:10 -0400

    fixed admin session template code missing people image issue

--- catalog/admin/includes/classes/template.php
+++ catalog/admin/includes/classes/template.php
@@ -158,6 +158,41 @@ class lC_Template_Admin extends lC_Template {
     }
 
     return $ok;
+  }  
+  /**
+  * Sets the template to use
+  *
+  * @param string $code The code of the template to use
+  * @access public
+  */
+  function set($code = null) {
+    if ( (isset($_SESSION['template']) === false) || !empty($code) || (isset($_GET['template']) && !empty($_GET['template'])) ) {
+      if ( !empty( $code ) ) {
+        $set_template = $code;
+      } else {
+        $set_template = (isset($_GET['template']) && !empty($_GET['template'])) ? $_GET['template'] : DEFAULT_TEMPLATE;
+      }
+
+      /*$data = array();
+      $data_default = array();
+
+      foreach ($this->getTemplates() as $template) {
+        if ($template['code'] == DEFAULT_TEMPLATE) {
+          $data_default = array('id' => $template['id'], 'code' => $template['code']);
+        } elseif ($template['code'] == $set_template) {
+          $data = array('id' => $template['id'], 'code' => $template['code']);
+        }
+      }
+
+      if (empty($data)) {
+        $data = $data_default;
+      }*/
+
+      $_SESSION['template']['code'] = $set_template;
+    }
+
+    //$this->_template_id = $_SESSION['template']['id'];
+    //$this->_template = $_SESSION['template']['code'];
   }
 }
 ?>
