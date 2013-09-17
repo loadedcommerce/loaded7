e17b9bde9bd148c286636f6c035edd76ff596ab7
datazen <scott.logsdon@loadedcommerce.com>
2013-09-17 11:28:50 -0400

    [IC] commented out not yet used areas

--- catalog/admin/includes/applications/store/pages/main.php
+++ catalog/admin/includes/applications/store/pages/main.php
@@ -41,7 +41,7 @@ foreach ($typesArr as $key => $value) {
 </style>
 <section role="main" id="main">
   <noscript class="message black-gradient simpler"><?php echo $lC_Language->get('ms_error_javascript_not_enabled_warning'); ?></noscript>
-  <hgroup id="main-title" class="thin" style="padding-bottom:0;">
+  <hgroup id="main-title" class="thin">
     <h1><?php echo $lC_Template->getPageTitle(); ?></h1>
     <?php /*
     <div class="columns margin-bottom" id="storeHeaderRightContainer">
