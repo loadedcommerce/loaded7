da300c2165be6d4c847d764e0a92e7c3f2308d14
wa4u <kiran@thereactorworks.com>
2013-10-05 01:27:13 +0530

    [D1607] Added image upload to Home Page Content ckeditor.

--- catalog/admin/includes/applications/branding_manager/js/branding_manager.js.php
+++ catalog/admin/includes/applications/branding_manager/js/branding_manager.js.php
@@ -23,7 +23,7 @@ $(document).ready(function() {
   <?php
   if (defined('MODULE_CONTENT_HOMEPAGE_HTML_CONTENT')) {
     if (ENABLE_EDITOR == '1') { 
-      echo "CKEDITOR.replace('branding_home_page_text', { height: 200, width: '99%' });";
+      echo "CKEDITOR.replace('branding_home_page_text', { height: 200, width: '99%', filebrowserUploadUrl: '../ext/jquery/ckeditor/ck_upload.php' });";
     } else {
       echo '$("#branding_home_page_text").css("height", "200px").css("width", "99.8%");';
     }
