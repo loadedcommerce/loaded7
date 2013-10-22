c88f4b8a9e8f8db464dfd1a27cc8ad9d8ebb4a4c
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-10-22 14:20:57 -0400

    updated for menu current navigable css

--- catalog/admin/templates/default/general.js.php
+++ catalog/admin/templates/default/general.js.php
@@ -80,14 +80,16 @@ $(document).ready(function() {
     } else if (module.indexOf("templates_modules_layout") != -1 && document.location.href.indexOf("set=content") != -1) {
       $("#big-menu_templates_modules_layout").addClass('current navigable-current').change();
       cfg = true;
-    } else if (module.indexOf("products") != -1 && document.location.href.indexOf("products") != -1) {
-      $("#big-menu_product_catalog").addClass('current navigable-current').change();
+    } else if (module.indexOf("products") != -1 && document.location.href.indexOf("products") != -1 && module.indexOf("expected") == -1) {
+      $("#big-menu_products_list").addClass('current navigable-current').change();
+    } else if (module.indexOf("products_expected") != -1 && document.location.href.indexOf("expected") != -1) {
+      $("#big-menu_products_expected").addClass('current navigable-current').change();
     } else if (module.indexOf("coupons") != -1 && document.location.href.indexOf("coupons") != -1) {
       $("#big-menu_coupon_manager").addClass('current navigable-current').change();
     } else if (module.indexOf("orders") != -1 && document.location.href.indexOf("action=save") != -1) {
       $("#big-menu_orders_list").addClass('current navigable-current').change();
     } else if (module.indexOf("product_variants") != -1 && document.location.href.indexOf("product_variants") != -1) {
-      $("#big-menu_option_variants").addClass('current navigable-current').change();
+      $("#big-menu_options_manager").addClass('current navigable-current').change();
     } else if (module.indexOf("statistics") != -1 && document.location.href.indexOf("statistics") != -1) {
       $("#big-menu_statistics").addClass('current navigable-current').change();
     } else if (module.indexOf("tax_classes") != -1 && document.location.href.indexOf("tax_classes") != -1) {
