41dabbb8998c0d66d5934ea1d3a141fbae85a31c
datazen <scott.logsdon@loadedcommerce.com>
2013-10-31 09:43:05 -0400

    1830 repaired issue with period in state field

--- catalog/includes/content/checkout/payment_address.php
+++ catalog/includes/content/checkout/payment_address.php
@@ -132,7 +132,7 @@
             $Qzone = $lC_Database->query('select zone_id from :table_zones where zone_country_id = :zone_country_id and zone_code like :zone_code');
             $Qzone->bindTable(':table_zones', TABLE_ZONES);
             $Qzone->bindInt(':zone_country_id', $_POST['country']);
-            $Qzone->bindValue(':zone_code', $_POST['state']);
+            $Qzone->bindValue(':zone_code', preg_replace('/[^A-Z\s]/', '', $_POST['state']));
             $Qzone->execute();
 
             if ($Qzone->numberOfRows() === 1) {
@@ -141,7 +141,7 @@
               $Qzone = $lC_Database->query('select zone_id from :table_zones where zone_country_id = :zone_country_id and zone_name like :zone_name');
               $Qzone->bindTable(':table_zones', TABLE_ZONES);
               $Qzone->bindInt(':zone_country_id', $_POST['country']);
-              $Qzone->bindValue(':zone_name', $_POST['state'] . '%');
+              $Qzone->bindValue(':zone_name', preg_replace('/[^A-Z\s]/', '', $_POST['state']) . '%');
               $Qzone->execute();
 
               if ($Qzone->numberOfRows() === 1) {
@@ -187,7 +187,7 @@
           $Qab->bindValue(':entry_suburb', ((ACCOUNT_SUBURB > -1) ? trim($_POST['suburb']) : ''));
           $Qab->bindValue(':entry_postcode', ((ACCOUNT_POST_CODE > -1) ? trim($_POST['postcode']) : ''));
           $Qab->bindValue(':entry_city', trim($_POST['city']));
-          $Qab->bindValue(':entry_state', ((ACCOUNT_STATE > -1) ? (($zone_id > 0) ? '' : trim($_POST['state'])) : ''));
+          $Qab->bindValue(':entry_state', ((ACCOUNT_STATE > -1) ? (($zone_id > 0) ? '' : preg_replace('/[^A-Z\s]/', '', $_POST['state'])) : ''));
           $Qab->bindInt(':entry_country_id', $_POST['country']);
           $Qab->bindInt(':entry_zone_id', ((ACCOUNT_STATE > -1) ? (($zone_id > 0) ? $zone_id : 0) : ''));
           $Qab->bindValue(':entry_telephone', ((ACCOUNT_TELEPHONE > -1) ? trim($_POST['telephone']) : ''));
