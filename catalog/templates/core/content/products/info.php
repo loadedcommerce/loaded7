6693856794229242b8aa4f0b962fa720208765cf
gulsarrays <gulmohar@loadedcommerce.com>
2013-11-12 20:17:26 +0530

    B1803 Product Additional Image - Unable to see from catalog

--- catalog/templates/core/content/products/info.php
+++ catalog/templates/core/content/products/info.php
@@ -33,12 +33,11 @@
           </div>
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-    <hr>
+    </div><!-- /.modal -->    
     <?php
     if (sizeof($lC_Product->getImages()) > 1) {
       $_product_additionalimages = $lC_Product->getAdditionalImagesHtml(); 
-      echo '<div class="thumbnail img-responsive"><ul class="pr_gallery">' . $_product_additionalimages['images'] . '</ul></div><hr>' . "\n";
+      echo '<div class="thumbnail img-responsive"><ul class="pr_gallery">' . $_product_additionalimages['images'] . '</ul></div>' . "\n";
       echo $_product_additionalimages['model']. "\n";
     }
     ?>
