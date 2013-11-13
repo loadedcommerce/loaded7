973f51dede6cf78ffb5e25e5b0b05ed61ad64ae9
jamielea <jamielealanders@gmail.com>
2013-11-13 19:00:27 +0000

    [B1874] Message stack for account success on checkout shipping page

--- catalog/includes/content/checkout/shipping.php
+++ catalog/includes/content/checkout/shipping.php
@@ -105,6 +105,10 @@ class lC_Checkout_Shipping extends lC_Template {
       }
     } 
     
+    if(isset($_GET['account_created'])){
+      $lC_MessageStack->add('checkout_shipping_account', $lC_Language->get('success_account_updated'), 'success');
+    }
+    
     if ($_GET[$this->_module] == 'process') {
       $this->_process();
     }
