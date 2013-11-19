be989be165f69b0c2b8f9e13ad028e1f943ba6c9
datazen <scott.logsdon@loadedcommerce.com>
2013-11-19 18:12:39 -0500

    [IC] updated set permissions logic

--- catalog/admin/includes/applications/updates/classes/updates.php
+++ catalog/admin/includes/applications/updates/classes/updates.php
@@ -645,22 +645,38 @@ class lC_Updates_Admin {
   * @access protected      
   * @return boolean
   */
-  protected static function chmod_r($path) {
-    $dp = opendir($path);
-    while($file = readdir($dp)) {
-      if($file != "." AND $file != "..") {
-        if(is_dir($file)){
-          chmod($file, 0755);
-          self::chmod_r($path . "/" . $file);
-        } else {
-          chmod($path . "/" . $file, 0644);
-        }
-      }
+  protected static function recursiveChmod ($path, $filePerm=0644, $dirPerm=0755) {
+    // Check if the path exists
+    if (!file_exists($path)) {
+      return(false);
     }
-    closedir($dp);
-    
-    return true;
-  }  
+
+    // See whether this is a file
+    if (is_file($path)) {
+      // Chmod the file with our given filepermissions
+      chmod($path, $filePerm);
+
+      // If this is a directory...
+    } elseif (is_dir($path)) {
+      // Then get an array of the contents
+      $foldersAndFiles = scandir($path);
+
+      // Remove "." and ".." from the list
+      $entries = array_slice($foldersAndFiles, 2);
+
+      // Parse every result...
+      foreach ($entries as $entry) {
+        // And call this function again recursively, with the same permissions
+        self::recursiveChmod($path."/".$entry, $filePerm, $dirPerm);
+      }
+
+      // When we are done with the contents of the directory, we chmod the directory itself
+      chmod($path, $dirPerm);
+    }
+
+    // Everything seemed to work out well, return true
+    return(true);
+  }
  /**
   * Check if a log exists
   *  
