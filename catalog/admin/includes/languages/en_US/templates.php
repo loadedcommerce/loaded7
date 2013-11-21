76a132ff29d3940754dcc6b405e5f10fac1cc800
datazen <scott.logsdon@loadedcommerce.com>
2013-11-21 17:29:01 -0500

    [IC] adjust template listing column names

--- catalog/admin/includes/languages/en_US/templates.php
+++ catalog/admin/includes/languages/en_US/templates.php
@@ -11,8 +11,8 @@
 
 heading_title = Templates
 
-table_heading_templates = Template Info
-table_heading_preview = Template Preview
+table_heading_templates = Information
+table_heading_preview = Preview
 table_heading_action = Action
 
 modal_heading_edit_template = Edit Template
