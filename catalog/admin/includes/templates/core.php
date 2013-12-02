749a6fdc2145a4785f0150f5c76d31e4977cd2cc
datazen <scott.logsdon@loadedcommerce.com>
2013-12-02 18:37:00 -0500

    renamed core screenshot

--- catalog/admin/includes/templates/core.php
+++ catalog/admin/includes/templates/core.php
@@ -17,7 +17,7 @@ class lC_Template_core {
       $_markup_version = 'HTML5/CSS3',
       $_css_based = '1', /* 0=No; 1=Yes */
       $_medium = 'Mobile Responsive UI',
-      $_screenshot = 'core_screenshot.png',
+      $_screenshot = 'core.png',
       $_version = '1.0.0',
       $_compatibility = '7.0',      
       $_groups = array('boxes' => array('left', 'right'),
