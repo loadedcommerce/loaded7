29f03d0e99e4acc4241a9f9a031a5ae46e246baa
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-12-02 10:44:03 -0500

    fix in class file

--- catalog/admin/includes/classes/order.php
+++ catalog/admin/includes/classes/order.php
@@ -241,54 +241,6 @@ class lC_Order {
     }
 
     $this->_products = $products_array;
-  }    
-
-      $Qproduct = $lC_Database->query('select products_id, products_name, products_model, products_price, products_tax, products_quantity, products_simple_options_meta_data from :table_orders_products where orders_products_id = :orders_products_id and orders_id = :orders_id limit 1');
-      $Qproduct->bindTable(':table_orders_products', TABLE_ORDERS_PRODUCTS);
-      $Qproduct->bindInt(':orders_products_id', $pid);
-      $Qproduct->bindInt(':orders_id', $oid);
-      $Qproduct->execute();
-      
-      while ($Qproduct->next()) {
-        $product_array[$key] = array('quantity' => $Qproduct->valueInt('products_quantity'),
-                                     'productID' => $Qproducts->value('products_id'),
-                                     'name' => $Qproduct->value('products_name'),
-                                     'model' => $Qproduct->value('products_model'),
-                                     'tax' => $Qproduct->value('products_tax'),
-                                     'price' => $Qproduct->value('products_price'),
-                                     'options' => unserialize($Qproduct->value('products_simple_options_meta_data')));
-
-    $Qproduct = $lC_Database->query('select products_name, products_model, products_price, products_tax, products_quantity, products_simple_options_meta_data from :table_orders_products where orders_products_id = :orders_products_id and orders_id = :orders_id limit 1');
-    $Qproduct->bindTable(':table_orders_products', TABLE_ORDERS_PRODUCTS);
-    $Qproduct->bindInt(':orders_products_id', $pid);
-    $Qproduct->bindInt(':orders_id', $oid);
-    $Qproduct->execute();
-    
-    while ($Qproduct->next()) {
-      $product_array[$key] = array('quantity' => $Qproduct->valueInt('products_quantity'),
-                                   'name' => $Qproduct->value('products_name'),
-                                   'model' => $Qproduct->value('products_model'),
-                                   'tax' => $Qproduct->value('products_tax'),
-                                   'price' => $Qproduct->value('products_price'),
-                                   'options' => unserialize($Qproduct->value('products_simple_options_meta_data')));
-
-      $Qvariants = $lC_Database->query('select group_title, value_title from :table_orders_products_variants where orders_id = :orders_id and orders_products_id = :orders_products_id order by id');
-      $Qvariants->bindTable(':table_orders_products_variants', TABLE_ORDERS_PRODUCTS_VARIANTS);
-      $Qvariants->bindInt(':orders_id', $this->_order_id);
-      $Qvariants->bindInt(':orders_products_id', $id);
-      $Qvariants->execute();
-
-      if ( $Qvariants->numberOfRows() > 0 ) {
-        while ( $Qvariants->next() ) {
-          $product_array[$key]['attributes'][] = array('option' => $Qvariants->value('group_title'),
-                                                        'value' => $Qvariants->value('value_title'));
-        }
-      }
-
-      $key++;
-    }
-
-    $this->_product = $product_array;
   }
 
   protected function _getTotals() {
