1eb1d7e0133ca1d08c65456e627e8ddd3a726255
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-12-05 15:56:06 -0500

    remove main info.php link from sitemap

--- catalog/templates/core/content/info/info_sitemap.php
+++ catalog/templates/core/content/info/info_sitemap.php
@@ -41,13 +41,9 @@ $lC_CategoryTree->setUseAria(true);
           <li><?php echo lc_link_object(lc_href_link(FILENAME_PRODUCTS, 'new'), $lC_Language->get('sitemap_products_new')); ?></li>
           <li><?php echo lc_link_object(lc_href_link(FILENAME_PRODUCTS, 'specials'), $lC_Language->get('sitemap_specials')); ?></li>
           <li><?php echo lc_link_object(lc_href_link(FILENAME_PRODUCTS, 'reviews'), $lC_Language->get('sitemap_reviews')); ?></li>
-          <li><?php echo lc_link_object(lc_href_link(FILENAME_INFO), $lC_Language->get('sitemap_information_pages')); ?>
-            <ul>
-              <li><?php echo lc_link_object(lc_href_link(FILENAME_INFO, 'cookie'), $lC_Language->get('breadcrumb_cookie_usage')); ?></li>
-              <li><?php echo lc_link_object(lc_href_link(FILENAME_INFO, 'ssl_check'), $lC_Language->get('breadcrumb_ssl_check')); ?></li>
-              <li><?php echo lc_link_object(lc_href_link(FILENAME_INFO, 'contact'), $lC_Language->get('box_information_contact')); ?></li>
-            </ul>
-          </li>
+          <li><?php echo lc_link_object(lc_href_link(FILENAME_INFO, 'cookie'), $lC_Language->get('breadcrumb_cookie_usage')); ?></li>
+          <li><?php echo lc_link_object(lc_href_link(FILENAME_INFO, 'ssl_check'), $lC_Language->get('breadcrumb_ssl_check')); ?></li>
+          <li><?php echo lc_link_object(lc_href_link(FILENAME_INFO, 'contact'), $lC_Language->get('box_information_contact')); ?></li>
         </ul>    
       </div>
     </div>    
