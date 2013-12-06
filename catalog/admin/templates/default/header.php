a404a7e53e3ff3046f3d3b007252ab377a95f5a0
datazen <scott.logsdon@loadedcommerce.com>
2013-12-06 13:21:58 -0500

    [IC] updated pro icon logic

--- catalog/admin/templates/default/header.php
+++ catalog/admin/templates/default/header.php
@@ -13,7 +13,11 @@
 <header role="banner" id="title-bar">
   <div class="relative">
     <a id="logoRef" style="" href="<?php echo lc_href_link_admin(FILENAME_DEFAULT); ?>"><img id="logoImg" src="./templates/default/img/logo.png" border="0"></a>
-    <!-- VQMOD1 -->
+    <?php 
+    if (utility::isPro() === true) {
+      echo '<span style="vertical-align:50%;"><span class="tag red-gradient glossy" style="padding-top:2px; font-size:1.0em;">PRO</span></span>';
+    }
+    ?>
   </div>
 </header>
 <?php
