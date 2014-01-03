06dc90c5e8f920055be1ac5644b301e2b7f93090
wa4u <kiran@thereactorworks.com>
2014-01-04 03:54:52 +0530

    [D2247] Updated code for language id.

--- catalog/admin/includes/applications/branding_manager/js/branding_manager.js.php
+++ catalog/admin/includes/applications/branding_manager/js/branding_manager.js.php
@@ -17,14 +17,14 @@ $(document).ready(function() {
   createogUploader();
  
   <?php
+  foreach ( $lC_Language->getAll() as $l ) {
+    $lid = $l['id'];
     if (ENABLE_EDITOR == '1') { 
-      foreach ( $lC_Language->getAll() as $l ) {
-        $lid = $l['id'];
-        echo "CKEDITOR.replace('branding_home_page_text' . $lid . '', { height: 200, width: '99%', filebrowserUploadUrl: '../ext/jquery/ckeditor/ck_upload.php' });";
-      }
+        echo "CKEDITOR.replace('branding_home_page_text[$lid]', { height: 200, width: '99%', filebrowserUploadUrl: '../ext/jquery/ckeditor/ck_upload.php' });";
     } else {
-      echo '$("#branding_home_page_text").css("height", "200px").css("width", "99.8%");';
+      echo '$("#branding_home_page_text[$lid]").css("height", "200px").css("width", "99.8%");';
     }
+  }
   ?>
 
   $(window).resize(function() {
