8352235627764d26d4d0b6e678e1fed71934296d
Gerald Bullard Jr <gerald@thereactorworks.com>
2014-01-17 19:46:52 -0500

    much more header work for 1.5 template

--- catalog/templates/core15/header.php
+++ catalog/templates/core15/header.php
@@ -14,79 +14,67 @@
 //print_r($_SESSION);
 //echo '</pre>'; 
 ?>
-<!--header.php start-->
-<div class="navbar navbar-inverse" role="navigation">   
-  <!-- Brand and toggle get grouped for better mobile display -->
+<!--header.php start-->     
+<div class="topnav mid-margin-bottom">
   <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-        <span class="sr-only"><?php echo $lC_Language->get('text_toggle_navigation'); ?></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse navbar-ex1-collapse">
-      <div class="pull-right">
-        <ul class="locale-menu nav navbar-nav">
-          <li class="dropdown">
+    <div class="pull-right">
+      <ul class="locale-menu nav navbar-nav">
+        <li class="dropdown">
+          <?php 
+            echo '<a class="dropdown-toggle" data-toggle="dropdown" href="#">' . 
+                    $lC_Language->showImage($value['code'], '18', '12', 'class="locale-header-icon"') . 
+                 '  <span class="locale-header-currency">' . 
+                      $lC_Currencies->getCode() . 
+                 '    (' . $lC_Currencies->getSessionSymbolLeft() . ')' . 
+                 '  </span>' .  
+                 '  <b class="caret"></b>' . 
+                 '</a>'; 
+          ?>
+          <ul class="dropdown-menu locale-header-dropdown">
+            <?php echo lC_Template_output::getTemplateLanguageSelection(true, true, ''); ?>
+            <li role="presentation" class="divider"></li>
+            <?php echo lC_Template_output::getTemplateCurrenciesSelection(true, true, ''); ?>
+          </ul>
+        </li>
+      </ul>
+      <ul class="account-menu nav navbar-nav">
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">My Account <b class="caret"></b></a>
+          <ul class="dropdown-menu account-dropdown">
+            <li><a href="<?php echo lc_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>"><?php echo $lC_Language->get('text_sign_in'); ?></a></li>
             <?php 
-              echo '<a class="dropdown-toggle" data-toggle="dropdown" href="#">' . 
-                      $lC_Language->showImage($value['code'], '18', '12', 'class="locale-header-icon"') . 
-                   '  <span class="locale-header-currency">' . 
-                        $lC_Currencies->getCode() . 
-                   '    (' . $lC_Currencies->getSessionSymbolLeft() . ')' . 
-                   '  </span>' .  
-                   '  <b class="caret"></b>' . 
-                   '</a>'; 
+            if ($lC_Customer->isLoggedOn()) { 
+              echo '<li><a href="' . lc_href_link(FILENAME_ACCOUNT, 'logoff', 'SSL') , '">' . $lC_Language->get('text_sign_out') . '</a></li>';
+            } 
             ?>
-            <ul class="dropdown-menu locale-header-dropdown">
-              <?php echo lC_Template_output::getTemplateLanguageSelection(true, true, ''); ?>
-              <li role="presentation" class="divider"></li>
-              <?php echo lC_Template_output::getTemplateCurrenciesSelection(true, true, ''); ?>
-            </ul>
-          </li>
-        </ul>
-        <ul class="account-menu nav navbar-nav">
-          <li class="dropdown">
-            <a class="dropdown-toggle white" data-toggle="dropdown" href="#">My Account <b class="caret white"></b></a>
-            <ul class="dropdown-menu account-dropdown">
-              <li><a href="<?php echo lc_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>"><?php echo $lC_Language->get('text_sign_in'); ?></a></li>
-              <?php 
-              if ($lC_Customer->isLoggedOn()) { 
-                echo '<li><a href="' . lc_href_link(FILENAME_ACCOUNT, 'logoff', 'SSL') , '">' . $lC_Language->get('text_sign_out') . '</a></li>';
-              } 
-              ?>
-              <li><a href="<?php echo lc_href_link(FILENAME_INFO, 'contact', 'NONSSL'); ?>"><?php echo $lC_Language->get('text_contact'); ?></a></li>
-              <li><a href="<?php echo lc_href_link(FILENAME_CHECKOUT, 'shipping', 'SSL'); ?>"><?php echo $lC_Language->get('text_checkout'); ?></a></li>
-            </ul>
-          </li>
-        </ul>
-        <ul class="cart-menu nav navbar-nav">
-          <li class="dropdown">
-            <a class="dropdown-toggle white" data-toggle="dropdown" href="#"><i class="fa fa-shopping-cart white small-margin-right"></i> (0) Items <b class="caret white"></b></a>
-            <ul class="dropdown-menu cart-dropdown">
-              Cart Contents Go Here
-            </ul>
-          </li>
-        </ul>   
-        <?php if ($lC_Template->getBranding('chat_code') != '') { ?>
-        <ul class="nav navbar-nav">
-          <li>
-            <?php echo $lC_Template->getBranding('chat_code'); ?>
-          </li>
-        </ul>  
-        <?php } ?>
-      </div>    
+            <li><a href="<?php echo lc_href_link(FILENAME_INFO, 'contact', 'NONSSL'); ?>"><?php echo $lC_Language->get('text_contact'); ?></a></li>
+            <li><a href="<?php echo lc_href_link(FILENAME_CHECKOUT, 'shipping', 'SSL'); ?>"><?php echo $lC_Language->get('text_checkout'); ?></a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="cart-menu nav navbar-nav">
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+            <i class="fa fa-shopping-cart white small-margin-right"></i> (0) Items <b class="caret"></b>
+          </a>
+          <ul class="dropdown-menu cart-dropdown">
+            <li><a>Cart Contents Go Here</a></li>
+          </ul>
+        </li>
+      </ul>   
+      <?php if ($lC_Template->getBranding('chat_code') != '') { ?>
+      <ul class="nav navbar-nav">
+        <li>
+          <?php echo $lC_Template->getBranding('chat_code'); ?>
+        </li>
+      </ul>  
+      <?php } ?>
     </div>
   </div>
 </div>
-
 <div class="page-header">
   <div class="container">
-    <div class="row">
+    <div class="row mid-margin-bottom">
       <div class="col-sm-3 col-lg-3">
         <h1 class="logo">
           <a href="<?php echo lc_href_link(FILENAME_DEFAULT, '', 'NONSSL'); ?>">
@@ -133,7 +121,27 @@
         </div>
       </div>       
     </div>
-    <div class="row small-margin-top">
+    <div class="navbar navbar-inverse no-margin-bottom with-small-padding" role="navigation">   
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only"><?php echo $lC_Language->get('text_toggle_navigation'); ?></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>    
+      <div class="no-margin-bottom">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse small-margin-top small-margin-bottom">
+          <ul class="nav navbar-nav">
+            <li><a href="<?php echo lc_href_link(FILENAME_DEFAULT, '', 'NONSSL'); ?>"><?php echo $lC_Language->get('text_home'); ?></a></li>
+            <?php echo $lC_Template->getTopCategoriesSelection(); ?>
+          </ul>    
+        </div>
+      </div>
+    </div>
+    <div class="small-margin-top">
       <?php
       if ($lC_Services->isStarted('breadcrumb')) {
         echo '<ol class="breadcrumb">' . $lC_Breadcrumb->getPathList() . '</ol>' . "\n";
