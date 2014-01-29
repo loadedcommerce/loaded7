924c0a618ee93f04248f25197cac0ccdd58282b0
Gerald Bullard Jr <gerald@thereactorworks.com>
2014-01-28 21:41:55 -0500

    added back reverted files

---
+++ catalog/templates/core/modules/boxes/languages.php
@@ -0,0 +1,29 @@
+<?php
+/**
+  @package    catalog::search::boxes
+  @author     Loaded Commerce
+  @copyright  Copyright 2003-2014 Loaded Commerce, LLC
+  @copyright  Portions Copyright 2003 osCommerce
+  @copyright  Template built on DevKit http://www.bootstraptor.com under GPL license 
+  @license    https://github.com/loadedcommerce/loaded7/blob/master/LICENSE.txt
+  @version    $Id: languages.php v1.0 2013-08-08 datazen $
+*/
+?>
+<!--modules/boxes/languages.php start-->
+<div class="well" >
+  <ul class="box-languages list-unstyled">
+    <li class="box-header small-margin-bottom"><?php echo $lC_Box->getTitle(); ?></li>
+    <?php echo $lC_Box->getContent(); ?>
+  </ul>
+</div>
+<script>
+$(document).ready(function() {
+  $('.box-languages li').each(function () {
+    if ($(this).is(':first-child')) {
+    } else {
+      $(this).addClass('text-center');
+    }
+  });
+});
+</script>
+<!--modules/boxes/languages.php end-->
---
+++ catalog/templates/core/modules/content/product_on_homepage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+  @package    catalog::templates::content
+  @author     Loaded Commerce
+  @copyright  Copyright 2003-2014 Loaded Commerce, LLC
+  @copyright  Portions Copyright 2003 osCommerce
+  @copyright  Template built on DevKit http://www.bootstraptor.com under GPL license 
+  @license    https://github.com/loadedcommerce/loaded7/blob/master/LICENSE.txt
+  @version    $Id: product_on_homepage.php v1.0 2013-08-08 Kiran $
+*/
+?>
+<!--modules/content/product-on-home-page.php start-->
+<div class="col-sm-12 col-lg-12">
+  <div class="row content-mainpage-content-container margin-bottom">
+    <?php echo $lC_Box->getContent(); ?>
+  </div>
+</div>
+<!--modules/content/product-on-home-page.php end-->
