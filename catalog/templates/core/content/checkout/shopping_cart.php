9007e618082c3fc20134e208817e9977e5d9898c
datazen <scott.logsdon@loadedcommerce.com>
2014-02-04 16:45:19 -0500

    2333 - corrected simple options

--- catalog/templates/core/content/checkout/shopping_cart.php
+++ catalog/templates/core/content/checkout/shopping_cart.php
@@ -45,14 +45,14 @@
                   echo '<h4 class="no-margin-top no-margin-bottom">' . lc_link_object(lc_href_link(FILENAME_PRODUCTS, $products['keyword']), $products['name']) . '</h4>' . "\n";
                   echo '<div class="clearfix primary">' . "\n";
                   if (!empty($products['model'])) {
-                    echo '<small>' . $lC_Language->get('listing_model_heading') . ': ' . $products['model'] . '</small>' . "\n";
+                    echo '<small>- ' . $lC_Language->get('listing_model_heading') . ': ' . $products['model'] . '</small>' . "\n";
                   }
                   if ( (STOCK_CHECK == '1') && ($lC_ShoppingCart->isInStock($products['id']) === false) ) {
                     echo '<span class="warning">' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . '</span>' . "\n";
                   }
                   if ( $lC_ShoppingCart->isVariant($products['item_id']) ) {
                     foreach ( $lC_ShoppingCart->getVariant($products['item_id']) as $variant) {
-                      echo '<br />- ' . $variant['group_title'] . ': ' . $variant['value_title'] . "\n";
+                      echo '<br /><small>- ' . $variant['group_title'] . ': ' . $variant['value_title'] . '</small>' . "\n";
                     }
                   }   
                   if ( $lC_ShoppingCart->hasSimpleOptions($products['item_id']) ) {
