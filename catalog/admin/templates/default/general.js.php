07e16b4c8f3c1f0b7c5804916cb838fae2b5d583
wa4u <kiran@thereactorworks.com>
2014-02-21 02:34:19 +0530

    [B2628] - Fixed Oembed causes run away JS error in Chrome.

--- catalog/admin/templates/default/general.js.php
+++ catalog/admin/templates/default/general.js.php
@@ -385,7 +385,7 @@ $(document).ready(function() {
   // defeat Google Chrome form autofill and its yellow background
   if(navigator.userAgent.toLowerCase().indexOf("chrome") >= 0 || navigator.userAgent.toLowerCase().indexOf("safari") >= 0){
     window.setInterval(function(){
-      $('input:-webkit-autofill').each(function(){
+      jQuery('input:-webkit-autofill').each(function(){
           var clone = $(this).clone(true, true);
           $(this).after(clone).remove();
       });
