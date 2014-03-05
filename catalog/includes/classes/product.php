9b9878b57dadbafc5eb0aade4078d416d0ef88c9
datazen <scott.logsdon@loadedcommerce.com>
2014-03-05 14:40:13 -0500

    [IC] corrected display with tax

--- catalog/includes/classes/product.php
+++ catalog/includes/classes/product.php
@@ -281,7 +281,7 @@ class lC_Product {
   
   //######## PRICING - ALL PRICING TO COME FROM HERE #########//
   public function getPriceInfo($product_id, $customers_group_id = 1, $data) {
-    global $lC_Specials, $lC_Database, $lC_Language, $lC_Customer, $lC_Services, $lC_Currencies;
+    global $lC_Specials, $lC_Database, $lC_Language, $lC_Customer, $lC_Services, $lC_Currencies, $lC_ShoppingCart, $lC_Tax;
 
     $quantity = (isset($data['quantity']) && $data['quantity'] != null) ? (int)$data['quantity'] : 1;
 
@@ -377,10 +377,19 @@ class lC_Product {
     
     $price = $price + $modTotal;
     
+    $tax = 0;
+    $taxRate = 0;
+    $priceWithTax = $price;
+    if(DISPLAY_PRICE_WITH_TAX == 1) { 
+      $taxRate = $lC_Tax->getTaxRate($lC_ShoppingCart->getShippingMethod('tax_class_id'), $lC_ShoppingCart->getShippingAddress('country_id'), $lC_ShoppingCart->getShippingAddress('zone_id'));
+      $tax = $lC_Tax->calculate($price, $taxRate);
+      $priceWithTax = lc_round($price + $tax, DECIMAL_PLACES);
+    }     
+    
     if ($lC_Services->isStarted('specials') && $lC_Specials->isActive($product_id)) {
-      $formatted = '<s>' . $lC_Currencies->displayPrice($this->getBasePrice() + $modTotal, $this->_data['tax_class_id']) . '</s> <span class="product-special-price">' . $lC_Currencies->displayPrice($price, $this->_data['tax_class_id']) . '</span>';
+      $formatted = '<s>' . $lC_Currencies->displayPrice($this->getBasePrice() + $modTotal + $tax, $this->_data['tax_class_id']) . '</s> <span class="product-special-price">' . $lC_Currencies->displayPrice($priceWithTax, $this->_data['tax_class_id']) . '</span>';
     } else {
-      $formatted = $lC_Currencies->displayPrice($price, $this->getTaxClassID());
+      $formatted = $lC_Currencies->displayPrice($priceWithTax, $this->getTaxClassID());
     }
     
     // #### DISCOUNTS #### //
@@ -392,9 +401,11 @@ class lC_Product {
       $baseline_discount = $lC_Customer->getBaselineDiscount($customers_group_id);
       $price = round((float)$base_price * ((float)$baseline_discount * .01), DECIMAL_PLACES); 
     }
-   */ 
+   */   
+   
     $return = array('base' => number_format($this->getBasePrice(), DECIMAL_PLACES),
                     'price' => number_format($price, DECIMAL_PLACES),
+                    'tax' => $tax,
                     'formatted' => $formatted,
                     'modTotal' => $modTotal,
                     'qpbData' => $qpbData
