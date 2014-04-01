cf0b924ecc725c067166211d35d9c2ba886dd781
datazen <scott.logsdon@loadedcommerce.com>
2014-04-01 16:12:58 -0400

    cleanup

--- catalog/includes/modules/variants/text_field.php
+++ catalog/includes/modules/variants/text_field.php
@@ -38,7 +38,7 @@ class lC_Variants_text_field extends lC_Variants_Abstract {
         $group_title = $val['group_title'];
                           
         $string .= '<div class="form-group margin-left">
-                      <label class="label-control">' . $val['group_title'] . '</label>
+                      <label class="label-control small-margin-top">' . $val['group_title'] . '</label>
                       <input class="form-control display-inline two-third-width mid-margin-left mid-margin-right float-right" type="text" name="variants[' . $data['group_id'] . '][' . $field['id'] . ']" value="" modifier="' . $val['price_modifier'] . '" onchange="refreshPrice();" id="simple_options_' . $group_id . '_' . $val['value_id'] . '">
                       <span class="small-margin-left">' . $price_formatted . '</span>
                     </div>';
@@ -49,7 +49,7 @@ class lC_Variants_text_field extends lC_Variants_Abstract {
       $string = '';
       foreach ( $data['data'] as $field ) {
         $string .= '<div class="form-group margin-left">
-                      <label class="label-control small-margin-top">' . $data['title']  . '</label>
+                      <label class="label-control">' . $data['title']  . '</label>
                       <input type="text" class="form-control display-inline two-third-width mid-margin-left mid-margin-right float-right" name="variants[' . $data['group_id'] . '][' . $field['id'] . ']" id="variants_' . $data['group_id'] . '_' . $field['id'] . '">
                     </div>';
       }
