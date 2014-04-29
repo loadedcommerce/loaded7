c7ab794a70b15c92af3efe2afb23fc0b53dc09f5
geraldbullard <gerald.bullard>
2014-04-29 17:41:22 -0400

    added phpinfo button and hidden div when install error exists on index

--- catalog/install/templates/pages/index.php
+++ catalog/install/templates/pages/index.php
@@ -466,9 +466,47 @@ TD { height:19px; }
             <?php echo addslashes($lC_Language->get('image_button_retry')); ?>
           </a>
         </div>
+        <div id="phpInfoContainer" class="large-margin-top large-margin-right" style="float:right">
+          <a href="index.php" class="button">
+            <span class="button-icon blue-gradient glossy"><span class="icon-info-round"></span></span>
+            <?php echo addslashes($lC_Language->get('image_button_phpinfo')); ?>
+          </a>
+        </div>
         <?php 
       }
       ?>
+    </div>
+    <div id="phpInfo" class="large-margin-top" style="display:none;">
+      <style type="text/css" scoped="scoped">
+        #phpinfo { background-color: #eeeeee; color: #000000; }
+        #phpinfo pre { margin: 0px; font-family: monospace; }
+        #phpinfo a:link { color: #000099; text-decoration: none; }
+        #phpinfo a:hover { text-decoration: underline; }
+        #phpinfo table { border-collapse: collapse; }
+        #phpinfo .center { text-align: center; }
+        #phpinfo .center table { margin-left: auto; margin-right: auto; text-align: left; }
+        #phpinfo .center th { text-align: center !important; }
+        #phpinfo td, th { border: 1px solid #000000; font-size: 75%; vertical-align: baseline; }
+        #phpinfo h1 {}
+        #phpinfo h2 {}
+        #phpinfo .p {}
+        #phpinfo .e { background-color: #ccccff; font-weight: bold; color: #000000; }
+        #phpinfo .h { background-color: #9999cc; font-weight: bold; color: #000000; }
+        #phpinfo .v { background-color: #cccccc; color: #000000; }
+        #phpinfo .vr { background-color: #cccccc; text-align: right; color: #000000; }
+        #phpinfo img { float: right; border: 0px; }
+        #phpinfo hr { width: 600px; background-color: #cccccc; border: 0px; height: 1px; color: #000000; margin: 0 auto 0 auto; }
+      </style>
+      <div id="phpinfo" style="padding-top:20px;">
+        <?php 
+          ob_start();
+          phpinfo();
+          $pinfo = ob_get_contents();
+          ob_end_clean();
+          // the name attribute "module_Zend Optimizer" of an anker-tag is not xhtml valide, so replace it with "module_Zend_Optimizer"
+          echo (str_replace ("module_Zend Optimizer", "module_Zend_Optimizer", preg_replace('%^.*<body>(.*)</body>.*$%ms', '$1', $pinfo)));
+        ?>
+      </div>
     </div>    
   </fieldset>  
 </form>
