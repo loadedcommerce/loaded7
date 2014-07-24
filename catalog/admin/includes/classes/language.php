e23ca52637ab497ae04ae3d7d8b5c473d389c107
datazen <scott.logsdon@loadedcommerce.com>
2014-07-24 14:33:25 -0400

    reverted previous language fix

--- catalog/admin/includes/classes/language.php
+++ catalog/admin/includes/classes/language.php
@@ -48,49 +48,8 @@ class lC_Language_Admin extends lC_Language {
     $this->_inject($contents);
 
     unset($contents);
-
-    $this->injectAdminAddonDefinitions($language_code);
   }
 
-  
-  public function injectAdminAddonDefinitions($language_code) {
-    global $lC_Addons;
-      
-    if ($language_code == null) $language_code = 'en_US';
-    
-    // inject the addons language defines
-    if (isset($lC_Addons)) {
-      $aoArr = $lC_Addons->getAddons();
-      if (is_array($aoArr)) {
-        foreach ($aoArr as $ao => $aoData) {          
-          // load tha main lang file
-          if (file_exists(DIR_FS_CATALOG . 'addons/' . $ao . '/admin/languages/' . $language_code . '.php')) {
-            $contents = file(DIR_FS_CATALOG . 'addons/' . $ao . '/admin/languages/' . $language_code . '.php');
-            $this->_inject($contents);
-          }
-          
-          unset($contents);
-          
-          // load all the module lang files
-          $lC_DirectoryListing = new lC_DirectoryListing(DIR_FS_CATALOG . 'addons/' . $ao . '/admin/languages/' . $language_code);
-          $lC_DirectoryListing->setIncludeDirectories(false);
-          $lC_DirectoryListing->setRecursive(true);
-          $lC_DirectoryListing->setStats(true);
-
-          $moduleFiles = $lC_DirectoryListing->getFiles(); 
-          
-          foreach ( $moduleFiles as $file ) {
-            if (file_exists($file['path'])) {
-              $contents = file($file['path']);
-              $this->_inject($contents);
-              unset($contents);
-            }            
-          }
-        }
-      }      
-    }    
-  }
-  
   private function _inject($contents) {
     
     $ini_array = array();
@@ -118,7 +77,6 @@ class lC_Language_Admin extends lC_Language {
 
     $this->_definitions = array_merge($this->_definitions, $ini_array);    
   }
-  
   
   public function injectDefinitions($file, $language_code = null) {
     if ( is_null($language_code) ) {
