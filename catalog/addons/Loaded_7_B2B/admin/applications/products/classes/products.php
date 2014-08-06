8549bf5423b3e1e23f558462107c08971027452b
datazen <scott.logsdon@loadedcommerce.com>
2014-08-06 14:17:12 -0400

    working on save issue

--- catalog/addons/Loaded_7_B2B/admin/applications/products/classes/products.php
+++ catalog/addons/Loaded_7_B2B/admin/applications/products/classes/products.php
@@ -32,6 +32,14 @@ class lC_Products_b2b_Admin extends lC_Products_pro_Admin {
     $error = false;
     
     $products_id = lC_Products_pro_Admin::save($id, $data);   
+    
+    // group pricing
+    if (isset($data['group_pricing']) && is_array($data['group_pricing'])) {
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+die('123');    
+    }
    
     return $products_id; 
   }
@@ -92,15 +100,60 @@ class lC_Products_b2b_Admin extends lC_Products_pro_Admin {
      
       $content .= '<div>' .
                   '  <label for="" class="label margin-right"><b>'. $value['customers_group_name'] .'</b></label>' .
-                  '  <input type="checkbox" name="enable_group_pricing[' . $value['customers_group_id'] . ']" class="switch medium margin-right" />' .
+                  '  <input type="checkbox" name="group_pricing[' . $value['customers_group_id'] . '][enable]" class="switch medium margin-right" />' .
                   '    <div class="inputs grey" style="display:inline; padding:8px 0;">' .
                   '      <span class="mid-margin-left no-margin-right">' . $lC_Currencies->getSymbolLeft() . '</span>' .
-                  '      <input type="text" onchange="" onfocus="this.select();" name="group_price[' . $value['customers_group_id'] . ']" id="group_price_' . $value['customers_group_id'] . '" value="' . number_format($discounted_price, DECIMAL_PLACES) . '" class="input-unstyled small-margin-right grey" style="width:60px;"/>' .
+                  '      <input type="text" onchange="" onfocus="this.select();" name="group_pricing[' . $value['customers_group_id'] . '][price]" id="group_pricing_' . $value['customers_group_id'] . '_price" value="' . number_format($discounted_price, DECIMAL_PLACES) . '" class="input-unstyled small-margin-right grey" style="width:60px;"/>' .
                   '    </div>' .
-                  '  <small class="input-info mid-margin-left no-wrap">' . $lC_Language->get('text_price') . '<span class="tag glossy mid-margin-left">-' . number_format($value['baseline_discount'], DECIMAL_PLACES) . '%</span><!-- if specials enabled /Special--></small>' . 
+                  '  <small class="input-info mid-margin-left no-wrap">' . $lC_Language->get('text_price') . '<span class="tag glossy mid-margin-left">-' . number_format($value['baseline_discount'], DECIMAL_PLACES) . '%</span></small>' . 
                   '</div>';
     }
     
     return $content;
-  } 
+  }
+ /*
+  *  Return the product simple options accordian price listing content
+  *
+  * @access public
+  * @return array
+  */
+  public static function getSpecialPricingContent() {
+    global $lC_Language, $lC_Currencies, $pInfo;
+    
+    $content = '';
+    $groups = lC_Customer_groups_Admin::getAll();
+    foreach($groups['entries'] as $key => $value) {
+      $base = (isset($pInfo)) ? (float)$pInfo->get('products_price') : 0.00;
+      $special = (isset($pInfo)) ? (float)$pInfo->get('products_special_price') : 0.00;
+      $discount = (isset($base) && $base > 0.00) ? round( ((($base - $special) / $base) * 100), DECIMAL_PLACES) : 0.00; 
+     
+      $content .= '<label for="products_special_pricing_enable' . $value['customers_group_id'] . '" class="label margin-right"><b>'. $value['customers_group_name'] .'</b></label>' .
+                  '<div class="columns">' .
+                  '  <div class="new-row-mobile twelve-columns twelve-columns-mobile mid-margin-bottom">' .
+                  '    <input id="products_special_pricing_pricing_' . $value['customers_group_id'] . '_enable" name="products_special_pricing[' . $value['customers_group_id'] . '][enable]" type="checkbox" class="margin-right medium switch"' . (($pInfo->get('products_special_status') != 0) ? ' checked' : '') . ' />' .
+                  '    <div class="inputs" style="display:inline; padding:8px 0;">' .
+                  '      <span class="mid-margin-left no-margin-right">' . $lC_Currencies->getSymbolLeft() . '</span>' .
+                  '      <input type="text" onfocus="this.select();" onchange="updatePricingDiscountDisplay();" name="products_special_pricing[' . $value['customers_group_id'] . '][price]" id="products_special_pricing_' . $value['customers_group_id'] . '_price" value="' . number_format($pInfo->get('products_special_price'), DECIMAL_PLACES) . '" class="sprice input-unstyled small-margin-right" style="width:60px;" />' .
+                  '    </div>' .
+                  '    <small class="input-info mid-margin-left no-wrap">' . $lC_Language->get('text_special_price') . '<span class="disctag tag glossy mid-margin-left">-' . number_format($discount, DECIMAL_PLACES) . '%</span></small>' .
+                  '  </div>' .
+                  '  <div class="new-row-mobile twelve-columns twelve-columns-mobile">' .
+                  '    <span class="nowrap margin-right">' .
+                  '      <span class="input small-margin-top">' .
+                  '        <input name="products_special_pricing[' . $value['customers_group_id'] . '][start_date]" id="products_special_pricing_' . $value['customers_group_id'] . '_start_date" type="text" placeholder="Start" class="input-unstyled datepicker" value="' . $pInfo->get('products_special_start_date') . '" style="width:97px;" />' .
+                  '      </span>' .
+                  '      <span class="icon-calendar icon-size2 small-margin-left"></span>' .
+                  '    </span>' .
+                  '    <span class="nowrap">' .
+                  '      <span class="input small-margin-top">' .
+                  '        <input name="products_special_pricing[' . $value['customers_group_id'] . '][expires_date]" id="products_special_pricing_' . $value['customers_group_id'] . '_expires_date" type="text" placeholder="End" class="input-unstyled datepicker" value="' . $pInfo->get('products_special_expires_date') . '" style="width:97px;" />' .
+                  '      </span>' .
+                  '      <span class="icon-calendar icon-size2 small-margin-left"></span>' .
+                  '    </span>' .
+                  '  </div>' .
+                  '</div>';
+    }
+    
+    return $content;   
+  }
 }
--- catalog/admin/includes/applications/products/actions/save.php
+++ catalog/admin/includes/applications/products/actions/save.php
@@ -59,6 +59,7 @@ class lC_Application_Products_Actions_save extends lC_Application_Products {
       
       // specials
       if ( isset($_POST['specials_pricing_switch']) && $_POST['specials_pricing_switch'] == 'on' ) {
+        if (is_array($_POST['products_special_pricing'])) $data['products_special_pricing'] = $_POST['products_special_pricing'];
         $data['specials_pricing_switch'] = 1;
         if ( isset($_POST['products_special_pricing_enable1']) ) $data['products_special_pricing_enable1'] = ($_POST['products_special_pricing_enable1'] == 'on' ? 1 : 0);
         if ( isset($_POST['products_special_price'][1]) ) $data['products_special_price1'] = $_POST['products_special_price'][1];
@@ -89,6 +90,9 @@ class lC_Application_Products_Actions_save extends lC_Application_Products {
       
       // access levels
       if (isset($_POST['access_levels'])) $data['access_levels'] = $_POST['access_levels'];
+      
+      // group pricing
+      if (isset($_POST['group_pricing'])) $data['group_pricing'] = $_POST['group_pricing'];     
 
       if ( $error === false ) {
         // the line below is used as a hook match point - do not not modify or remove
