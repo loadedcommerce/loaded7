d16206864ade082ab3ea249f27a4eca456667583
datazen <scott.logsdon@loadedcommerce.com>
2014-09-05 10:29:32 -0400

    merge work

--- catalog/admin/includes/languages/en_US/modules/statistics/sales_tax_report.php
+++ catalog/admin/includes/languages/en_US/modules/statistics/sales_tax_report.php
@@ -7,6 +7,7 @@
 
 statistics_sales_tax_report_title = Sales Tax Report
 
+statistics_sales_tax_table_heading_year = Year
 statistics_sales_tax_table_heading_month = Month
 statistics_sales_tax_table_heading_day = Day
 statistics_sales_tax_table_heading_gross_income = Gross Income
