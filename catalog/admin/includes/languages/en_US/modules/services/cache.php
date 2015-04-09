e0d7ab7c491eb1e4df38d69a2f963447cc63a2c6
datazen <scott.logsdon@loadedcommerce.com>
2015-04-08 20:12:14 -0400

    [IC] cache switch

---
+++ catalog/admin/includes/languages/en_US/modules/services/cache.php
@@ -0,0 +1,9 @@
+#  @package    admin::languages::modules
+#  @author     Loaded Commerce
+#  @copyright  Copyright 2003-2014 Loaded Commerce, LLC
+#  @copyright  Portions Copyright 2003 osCommerce
+#  @license    https://github.com/loadedcommerce/loaded7/blob/master/LICENSE.txt
+#  @version    $Id: cache.php v1.0 2013-08-08 datazen $
+
+services_cache_title = Cache
+services_cache_description = Cache for faster operation
